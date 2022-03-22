9d0a492cf13d3ef81166893abfca8e400e7155df
nimakarimipour <karimipour.nima@gmail.com>
2022-03-21 23:00:32 -0700

    Final Result

--- src/de/gurkenlabs/litiengine/resources/NamedResource.java
+++ src/de/gurkenlabs/litiengine/resources/NamedResource.java
@@ -1,20 +1,23 @@
 package de.gurkenlabs.litiengine.resources;
 
+import de.gurkenlabs.litiengine.Initializer;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlTransient;
 
 public abstract class NamedResource implements Resource {
-  @XmlAttribute
-  private String name;
 
-  @XmlTransient
-  @Override
-  public String getName() {
-    return this.name;
-  }
+    @XmlAttribute
+    private String name;
 
-  @Override
-  public void setName(final String n) {
-    this.name = n;
-  }
+    @XmlTransient
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    @Initializer
+    public void setName(final String n) {
+        this.name = n;
+    }
 }
