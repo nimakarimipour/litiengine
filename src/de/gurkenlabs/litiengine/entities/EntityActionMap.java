080339aa9b643efca030f6a20b4642f85eadc322
nimakarimipour <karimipour.nima@gmail.com>
2022-03-28 22:28:10 -0700

    Final Result

--- src/de/gurkenlabs/litiengine/entities/EntityActionMap.java
+++ src/de/gurkenlabs/litiengine/entities/EntityActionMap.java
@@ -1,60 +1,59 @@
 package de.gurkenlabs.litiengine.entities;
 
+import javax.annotation.Nullable;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public final class EntityActionMap {
-  private final Map<String, EntityAction> actions;
 
-  EntityActionMap() {
-    this.actions = new ConcurrentHashMap<>();
-  }
+    private final Map<String, EntityAction> actions;
 
-  public EntityAction register(String name, Runnable action) {
-    if (name == null || name.isEmpty() || action == null) {
-      return null;
-    }
-    
-    EntityAction entityAction = new EntityAction(name, action);
-    this.actions.put(name, entityAction);
-    return entityAction;
-  }
-
-  public void register(EntityAction action) {
-    if (action == null) {
-      return;
+    EntityActionMap() {
+        this.actions = new ConcurrentHashMap<>();
     }
 
-    this.actions.put(action.getName(), action);
-  }
-
-  public void unregister(EntityAction action) {
-    if (action == null) {
-      return;
+    @Nullable
+    public EntityAction register(String name, Runnable action) {
+        if (name == null || name.isEmpty() || action == null) {
+            return null;
+        }
+        EntityAction entityAction = new EntityAction(name, action);
+        this.actions.put(name, entityAction);
+        return entityAction;
     }
 
-    this.unregister(action.getName());
-  }
-
-  public void unregister(String actionName) {
-    if (actionName == null || actionName.isEmpty()) {
-      return;
+    public void register(EntityAction action) {
+        if (action == null) {
+            return;
+        }
+        this.actions.put(action.getName(), action);
     }
 
-    this.actions.remove(actionName);
-  }
+    public void unregister(EntityAction action) {
+        if (action == null) {
+            return;
+        }
+        this.unregister(action.getName());
+    }
 
-  public Collection<EntityAction> getActions() {
-    return Collections.unmodifiableCollection(this.actions.values());
-  }
+    public void unregister(String actionName) {
+        if (actionName == null || actionName.isEmpty()) {
+            return;
+        }
+        this.actions.remove(actionName);
+    }
 
-  public EntityAction get(String actionName) {
-    return this.actions.getOrDefault(actionName, null);
-  }
+    public Collection<EntityAction> getActions() {
+        return Collections.unmodifiableCollection(this.actions.values());
+    }
 
-  public boolean exists(String actionName) {
-    return this.actions.containsKey(actionName);
-  }
+    public EntityAction get(String actionName) {
+        return this.actions.getOrDefault(actionName, null);
+    }
+
+    public boolean exists(String actionName) {
+        return this.actions.containsKey(actionName);
+    }
 }
--- src/de/gurkenlabs/litiengine/graphics/emitters/xml/EmitterLoader.java
+++ src/de/gurkenlabs/litiengine/graphics/emitters/xml/EmitterLoader.java
@@ -1,66 +1,65 @@
 package de.gurkenlabs.litiengine.graphics.emitters.xml;
 
+import javax.annotation.Nullable;
 import java.net.URL;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.xml.bind.JAXBException;
-
 import de.gurkenlabs.litiengine.resources.Resources;
 import de.gurkenlabs.litiengine.util.io.XmlUtilities;
 
 public class EmitterLoader {
-  private static final Map<String, EmitterData> loadedEmitters;
-  private static final Logger log = Logger.getLogger(EmitterLoader.class.getName());
 
-  private EmitterLoader() {
-  }
+    private static final Map<String, EmitterData> loadedEmitters;
 
-  static {
-    loadedEmitters = new ConcurrentHashMap<>();
-  }
+    private static final Logger log = Logger.getLogger(EmitterLoader.class.getName());
 
-  public static EmitterData load(String emitterXml) {
-    if (loadedEmitters.containsKey(emitterXml)) {
-      return loadedEmitters.get(emitterXml);
+    private EmitterLoader() {
     }
 
-    return load(Resources.getLocation(emitterXml));
-  }
-
-  public static EmitterData load(URL emitterXml) {
-    final String name = emitterXml.getFile();
-    if (loadedEmitters.containsKey(name)) {
-      return loadedEmitters.get(name);
+    static {
+        loadedEmitters = new ConcurrentHashMap<>();
     }
 
-    EmitterData loaded;
-    try {
-      loaded = XmlUtilities.read(EmitterData.class, emitterXml);
-    } catch (JAXBException e) {
-      log.log(Level.SEVERE, String.format("Failed to load emitter data for %s", emitterXml), e);
-      return null;
+    @Nullable
+    public static EmitterData load(String emitterXml) {
+        if (loadedEmitters.containsKey(emitterXml)) {
+            return loadedEmitters.get(emitterXml);
+        }
+        return load(Resources.getLocation(emitterXml));
     }
 
-    return load(loaded);
-  }
-
-  public static EmitterData load(EmitterData emitterData) {
-    if (loadedEmitters.containsKey(emitterData.getName())) {
-      return loadedEmitters.get(emitterData.getName());
+    @Nullable
+    public static EmitterData load(URL emitterXml) {
+        final String name = emitterXml.getFile();
+        if (loadedEmitters.containsKey(name)) {
+            return loadedEmitters.get(name);
+        }
+        EmitterData loaded;
+        try {
+            loaded = XmlUtilities.read(EmitterData.class, emitterXml);
+        } catch (JAXBException e) {
+            log.log(Level.SEVERE, String.format("Failed to load emitter data for %s", emitterXml), e);
+            return null;
+        }
+        return load(loaded);
     }
 
-    loadedEmitters.put(emitterData.getName(), emitterData);
-    return emitterData;
-  }
-
-  public static EmitterData get(String name) {
-    if (loadedEmitters.containsKey(name)) {
-      return loadedEmitters.get(name);
+    public static EmitterData load(EmitterData emitterData) {
+        if (loadedEmitters.containsKey(emitterData.getName())) {
+            return loadedEmitters.get(emitterData.getName());
+        }
+        loadedEmitters.put(emitterData.getName(), emitterData);
+        return emitterData;
     }
-    return null;
-  }
 
+    @Nullable
+    public static EmitterData get(String name) {
+        if (loadedEmitters.containsKey(name)) {
+            return loadedEmitters.get(name);
+        }
+        return null;
+    }
 }
