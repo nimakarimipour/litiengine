d1706f58d8172342a2b87ebe12458a644d622355
nimakarimipour <karimipour.nima@gmail.com>
2022-04-07 15:41:56 -0700

    Final Result

--- src/de/gurkenlabs/litiengine/environment/CreatureMapObjectLoader.java
+++ src/de/gurkenlabs/litiengine/environment/CreatureMapObjectLoader.java
@@ -1,5 +1,6 @@
 package de.gurkenlabs.litiengine.environment;
 
+import javax.annotation.Nullable;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -7,7 +8,6 @@ import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import de.gurkenlabs.litiengine.Direction;
 import de.gurkenlabs.litiengine.entities.AnimationInfo;
 import de.gurkenlabs.litiengine.entities.Creature;
@@ -18,104 +18,101 @@ import de.gurkenlabs.litiengine.environment.tilemap.MapObjectType;
 import de.gurkenlabs.litiengine.graphics.animation.EntityAnimationController;
 
 public class CreatureMapObjectLoader extends MapObjectLoader {
-  private static final Logger log = Logger.getLogger(CreatureMapObjectLoader.class.getName());
-  private static final List<Class<? extends Creature>> customCreatureType;
 
-  static {
-    customCreatureType = new CopyOnWriteArrayList<>();
-  }
+    private static final Logger log = Logger.getLogger(CreatureMapObjectLoader.class.getName());
 
-  protected CreatureMapObjectLoader() {
-    super(MapObjectType.CREATURE);
-  }
+    private static final List<Class<? extends Creature>> customCreatureType;
 
-  /**
-   * <p>
-   * Registers a custom {@link Creature} implementation that can be
-   * automatically provided by this {@link MapObjectLoader}.
-   * </p>
-   * 
-   * <p>
-   * <b>This should only be used if the particular implementation doesn't
-   * require any additional map object properties to be initialized.</b>
-   * </p>
-   * 
-   * Make sure that the implementation has the following present:
-   * <ol>
-   * <li>An {@link AnimationInfo} annotation with one or more sprite prefixes
-   * defined</li>
-   * <li>Either an empty constructor or a constructor that takes in the sprite
-   * prefix from the loader.</li>
-   * </ol>
-   * 
-   * <p>
-   * The latter is particularly useful for classes that can have different
-   * sprite sheets, i.e. share the same logic but might have a different
-   * appearance.
-   * </p>
-   * 
-   * @param <T>
-   *          The type of the custom creature implementation.
-   * @param creatureType
-   *          The class of the custom {@link Creature} implementation.
-   */
-  public static <T extends Creature> void registerCustomCreatureType(Class<T> creatureType) {
-    customCreatureType.add(creatureType);
-  }
-
-  @Override
-  public Collection<IEntity> load(Environment environment, IMapObject mapObject) {
-    Collection<IEntity> entities = new ArrayList<>();
-    if (!this.isMatchingType(mapObject)) {
-      return entities;
+    static {
+        customCreatureType = new CopyOnWriteArrayList<>();
     }
 
-    final String spriteSheet = mapObject.getStringValue(MapObjectProperty.SPRITESHEETNAME);
-
-    Creature creature = this.createNewCreature(mapObject, spriteSheet);
-    loadDefaultProperties(creature, mapObject);
-
-    if (mapObject.hasCustomProperty(MapObjectProperty.MOVEMENT_VELOCITY)) {
-      creature.setVelocity(mapObject.getFloatValue(MapObjectProperty.MOVEMENT_VELOCITY));
+    protected CreatureMapObjectLoader() {
+        super(MapObjectType.CREATURE);
     }
 
-    creature.setFacingDirection(mapObject.getEnumValue(MapObjectProperty.SPAWN_DIRECTION, Direction.class, Direction.RIGHT));
+    /**
+     * <p>
+     * Registers a custom {@link Creature} implementation that can be
+     * automatically provided by this {@link MapObjectLoader}.
+     * </p>
+     *
+     * <p>
+     * <b>This should only be used if the particular implementation doesn't
+     * require any additional map object properties to be initialized.</b>
+     * </p>
+     *
+     * Make sure that the implementation has the following present:
+     * <ol>
+     * <li>An {@link AnimationInfo} annotation with one or more sprite prefixes
+     * defined</li>
+     * <li>Either an empty constructor or a constructor that takes in the sprite
+     * prefix from the loader.</li>
+     * </ol>
+     *
+     * <p>
+     * The latter is particularly useful for classes that can have different
+     * sprite sheets, i.e. share the same logic but might have a different
+     * appearance.
+     * </p>
+     *
+     * @param <T>
+     *          The type of the custom creature implementation.
+     * @param creatureType
+     *          The class of the custom {@link Creature} implementation.
+     */
+    public static <T extends Creature> void registerCustomCreatureType(Class<T> creatureType) {
+        customCreatureType.add(creatureType);
+    }
 
-    entities.add(creature);
-    return entities;
-  }
-
-  protected Creature createNewCreature(IMapObject mapObject, String spriteSheet) {
-    // for each known custom creature type, check if it was registered for the specified spriteSheetName
-    // if so: create an instance of the custom class instead of the default Creature class
-    for (Class<? extends Creature> customCreature : customCreatureType) {
-      for (String prefix : EntityAnimationController.getDefaultSpritePrefixes(customCreature)) {
-        if (prefix != null && spriteSheet.equalsIgnoreCase(prefix)) {
-          Creature created = createCustomCreature(customCreature, spriteSheet);
-          if (created != null) {
-            return created;
-          }
+    @Override
+    public Collection<IEntity> load(Environment environment, IMapObject mapObject) {
+        Collection<IEntity> entities = new ArrayList<>();
+        if (!this.isMatchingType(mapObject)) {
+            return entities;
         }
-      }
+        final String spriteSheet = mapObject.getStringValue(MapObjectProperty.SPRITESHEETNAME);
+        Creature creature = this.createNewCreature(mapObject, spriteSheet);
+        loadDefaultProperties(creature, mapObject);
+        if (mapObject.hasCustomProperty(MapObjectProperty.MOVEMENT_VELOCITY)) {
+            creature.setVelocity(mapObject.getFloatValue(MapObjectProperty.MOVEMENT_VELOCITY));
+        }
+        creature.setFacingDirection(mapObject.getEnumValue(MapObjectProperty.SPAWN_DIRECTION, Direction.class, Direction.RIGHT));
+        entities.add(creature);
+        return entities;
     }
 
-    // if no custom creature type war registered for the spriteSheet, we just create a new Creature instance
-    return new Creature(spriteSheet);
-  }
-
-  private static Creature createCustomCreature(Class<? extends Creature> customCreature, String spriteSheet) {
-    try {
-      return customCreature.getConstructor(String.class).newInstance(spriteSheet);
-    } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
-      try {
-        Creature creature = customCreature.getConstructor().newInstance();
-        creature.setSpritesheetName(spriteSheet);
-        return creature;
-      } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException ex) {
-        log.log(Level.WARNING, "Could not automatically create creature of type {0} because a matching constructor is missing.", new Object[] { customCreature });
-        log.log(Level.SEVERE, ex.getMessage(), ex);
-      }
+    protected Creature createNewCreature(IMapObject mapObject, @Nullable String spriteSheet) {
+        // for each known custom creature type, check if it was registered for the specified spriteSheetName
+        // if so: create an instance of the custom class instead of the default Creature class
+        for (Class<? extends Creature> customCreature : customCreatureType) {
+            for (String prefix : EntityAnimationController.getDefaultSpritePrefixes(customCreature)) {
+                if (prefix != null && spriteSheet.equalsIgnoreCase(prefix)) {
+                    Creature created = createCustomCreature(customCreature, spriteSheet);
+                    if (created != null) {
+                        return created;
+                    }
+                }
+            }
+        }
+        // if no custom creature type war registered for the spriteSheet, we just create a new Creature instance
+        return new Creature(spriteSheet);
     }
-    return null;
-  }
+
+    @Nullable
+    private static Creature createCustomCreature(Class<? extends Creature> customCreature, String spriteSheet) {
+        try {
+            return customCreature.getConstructor(String.class).newInstance(spriteSheet);
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+            try {
+                Creature creature = customCreature.getConstructor().newInstance();
+                creature.setSpritesheetName(spriteSheet);
+                return creature;
+            } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException ex) {
+                log.log(Level.WARNING, "Could not automatically create creature of type {0} because a matching constructor is missing.", new Object[] { customCreature });
+                log.log(Level.SEVERE, ex.getMessage(), ex);
+            }
+        }
+        return null;
+    }
 }
