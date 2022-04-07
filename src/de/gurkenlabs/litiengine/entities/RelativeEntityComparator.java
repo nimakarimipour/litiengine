983286826a876351b38c957bc12a6e36c3514d63
nimakarimipour <karimipour.nima@gmail.com>
2022-04-07 15:17:08 -0700

    Final Result

--- src/de/gurkenlabs/litiengine/entities/RelativeEntityComparator.java
+++ src/de/gurkenlabs/litiengine/entities/RelativeEntityComparator.java
@@ -1,22 +1,26 @@
 package de.gurkenlabs.litiengine.entities;
 
+import javax.annotation.Nullable;
 import java.util.Comparator;
 
 public abstract class RelativeEntityComparator implements Comparator<IEntity> {
-  private IEntity relativeEntity;
 
-  protected RelativeEntityComparator() {
-  }
+    @Nullable
+    private IEntity relativeEntity;
 
-  protected RelativeEntityComparator(final IEntity relativeEntity) {
-    this.relativeEntity = relativeEntity;
-  }
+    protected RelativeEntityComparator() {
+    }
 
-  public IEntity getRelativeEntity() {
-    return this.relativeEntity;
-  }
+    protected RelativeEntityComparator(final IEntity relativeEntity) {
+        this.relativeEntity = relativeEntity;
+    }
 
-  public void setRelativeEntity(final IEntity relativeEntity) {
-    this.relativeEntity = relativeEntity;
-  }
+    @Nullable
+    public IEntity getRelativeEntity() {
+        return this.relativeEntity;
+    }
+
+    public void setRelativeEntity(final IEntity relativeEntity) {
+        this.relativeEntity = relativeEntity;
+    }
 }
