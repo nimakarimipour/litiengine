7a353903860d609449fd6fceda80796255a2d6da
nimakarimipour <karimipour.nima@gmail.com>
2022-04-08 01:24:00 -0700

    Final Result

--- src/de/gurkenlabs/litiengine/graphics/animation/PropAnimationController.java
+++ src/de/gurkenlabs/litiengine/graphics/animation/PropAnimationController.java
@@ -1,8 +1,8 @@
 package de.gurkenlabs.litiengine.graphics.animation;
 
+import javax.annotation.Nullable;
 import java.awt.image.BufferedImage;
 import java.util.Optional;
-
 import de.gurkenlabs.litiengine.entities.Prop;
 import de.gurkenlabs.litiengine.entities.PropState;
 import de.gurkenlabs.litiengine.entities.Rotation;
@@ -11,146 +11,136 @@ import de.gurkenlabs.litiengine.resources.Resources;
 import de.gurkenlabs.litiengine.util.Imaging;
 
 public class PropAnimationController<T extends Prop> extends EntityAnimationController<T> {
-  public static final String PROP_IDENTIFIER = "prop-";
 
-  /**
-   * Initializes a new instance of the {@code PropAnimationController} class.
-   * 
-   * @param prop
-   *          The prop related to this controller.
-   */
-  public PropAnimationController(final T prop) {
-    super(prop);
+    public static final String PROP_IDENTIFIER = "prop-";
 
-    this.setDefault(createAnimation(this.getEntity(), PropState.INTACT));
-    this.add(createAnimation(this.getEntity(), PropState.DAMAGED));
-    this.add(createAnimation(this.getEntity(), PropState.DESTROYED));
-  }
-
-  /**
-   * Gets the sprite name for the specified prop and state.
-   * 
-   * @param prop
-   *          The prop to retrieve the sprite name for.
-   * 
-   * @param appendState
-   *          A flag indicating whether the state should be appended to the name.
-   * 
-   * @return A string representing the sprite name for the specified prop in its state.
-   * 
-   * @see Prop#getSpritesheetName()
-   * @see Prop#getState()
-   */
-  public static String getSpriteName(final Prop prop, boolean appendState) {
-    return getSpriteName(prop, prop.getState(), appendState);
-  }
-
-  /**
-   * Gets the sprite name for the specified prop and state.
-   * 
-   * @param prop
-   *          The prop to retrieve the sprite name for.
-   * 
-   * @param state
-   *          The state of the prop.
-   * 
-   * @param appendState
-   *          A flag indicating whether the state should be appended to the name.
-   * 
-   * @return A string representing the sprite name for the specified prop in its state.
-   * 
-   * @see Prop#getSpritesheetName()
-   * @see Prop#getState()
-   */
-  public static String getSpriteName(final Prop prop, PropState state, boolean appendState) {
-    StringBuilder sb = new StringBuilder(PROP_IDENTIFIER);
-    sb.append(prop.getSpritesheetName());
-    if (appendState) {
-      sb.append("-");
-      sb.append(state.spriteString());
+    /**
+     * Initializes a new instance of the {@code PropAnimationController} class.
+     *
+     * @param prop
+     *          The prop related to this controller.
+     */
+    public PropAnimationController(final T prop) {
+        super(prop);
+        this.setDefault(createAnimation(this.getEntity(), PropState.INTACT));
+        this.add(createAnimation(this.getEntity(), PropState.DAMAGED));
+        this.add(createAnimation(this.getEntity(), PropState.DESTROYED));
     }
 
-    return sb.toString();
-  }
-
-  @Override
-  public BufferedImage getCurrentImage() {
-    final Animation animation = this.getCurrent();
-    if (animation == null || animation.getSpritesheet() == null) {
-      return null;
+    /**
+     * Gets the sprite name for the specified prop and state.
+     *
+     * @param prop
+     *          The prop to retrieve the sprite name for.
+     *
+     * @param appendState
+     *          A flag indicating whether the state should be appended to the name.
+     *
+     * @return A string representing the sprite name for the specified prop in its state.
+     *
+     * @see Prop#getSpritesheetName()
+     * @see Prop#getState()
+     */
+    public static String getSpriteName(final Prop prop, boolean appendState) {
+        return getSpriteName(prop, prop.getState(), appendState);
     }
 
-    String cacheKey = this.buildCurrentCacheKey();
-    cacheKey += "_" + this.getEntity().isAddShadow();
-    cacheKey += "_" + this.getEntity().getState();
-    cacheKey += "_" + this.getEntity().getSpriteRotation();
-    cacheKey += "_" + this.getEntity().flipHorizontally();
-    cacheKey += "_" + this.getEntity().flipVertically();
-    Optional<BufferedImage> opt = Resources.images().tryGet(cacheKey);
-    if (opt.isPresent()) {
-      return opt.get();
+    /**
+     * Gets the sprite name for the specified prop and state.
+     *
+     * @param prop
+     *          The prop to retrieve the sprite name for.
+     *
+     * @param state
+     *          The state of the prop.
+     *
+     * @param appendState
+     *          A flag indicating whether the state should be appended to the name.
+     *
+     * @return A string representing the sprite name for the specified prop in its state.
+     *
+     * @see Prop#getSpritesheetName()
+     * @see Prop#getState()
+     */
+    public static String getSpriteName(final Prop prop, PropState state, boolean appendState) {
+        StringBuilder sb = new StringBuilder(PROP_IDENTIFIER);
+        sb.append(prop.getSpritesheetName());
+        if (appendState) {
+            sb.append("-");
+            sb.append(state.spriteString());
+        }
+        return sb.toString();
     }
 
-    BufferedImage currentImage = super.getCurrentImage();
-    if (currentImage == null) {
-      return null;
+    @Override
+    @Nullable
+    public BufferedImage getCurrentImage() {
+        final Animation animation = this.getCurrent();
+        if (animation == null || animation.getSpritesheet() == null) {
+            return null;
+        }
+        String cacheKey = this.buildCurrentCacheKey();
+        cacheKey += "_" + this.getEntity().isAddShadow();
+        cacheKey += "_" + this.getEntity().getState();
+        cacheKey += "_" + this.getEntity().getSpriteRotation();
+        cacheKey += "_" + this.getEntity().flipHorizontally();
+        cacheKey += "_" + this.getEntity().flipVertically();
+        Optional<BufferedImage> opt = Resources.images().tryGet(cacheKey);
+        if (opt.isPresent()) {
+            return opt.get();
+        }
+        BufferedImage currentImage = super.getCurrentImage();
+        if (currentImage == null) {
+            return null;
+        }
+        if (this.getEntity().getSpriteRotation() != Rotation.NONE) {
+            currentImage = Imaging.rotate(currentImage, this.getEntity().getSpriteRotation());
+        }
+        if (this.getEntity().flipHorizontally()) {
+            currentImage = Imaging.horizontalFlip(currentImage);
+        }
+        if (this.getEntity().flipVertically()) {
+            currentImage = Imaging.verticalFlip(currentImage);
+        }
+        if (!this.getEntity().isAddShadow()) {
+            return currentImage;
+        }
+        // add a shadow at the lower end of the current sprite.
+        final int ShadowYOffset = currentImage.getHeight();
+        final BufferedImage shadow = Imaging.addShadow(currentImage, 0, ShadowYOffset);
+        Resources.images().add(cacheKey, shadow);
+        return shadow;
     }
 
-    if (this.getEntity().getSpriteRotation() != Rotation.NONE) {
-      currentImage = Imaging.rotate(currentImage, this.getEntity().getSpriteRotation());
+    @Override
+    public void update() {
+        super.update();
+        this.play(this.getEntity().getState().spriteString());
     }
 
-    if (this.getEntity().flipHorizontally()) {
-      currentImage = Imaging.horizontalFlip(currentImage);
+    @Override
+    public boolean isAutoScaling() {
+        return this.getEntity().isScaling();
     }
 
-    if (this.getEntity().flipVertically()) {
-      currentImage = Imaging.verticalFlip(currentImage);
+    @Nullable
+    private static Animation createAnimation(final Prop prop, final PropState state) {
+        final Spritesheet spritesheet = findSpriteSheet(prop, state);
+        if (spritesheet == null) {
+            return null;
+        }
+        return new Animation(state.spriteString(), spritesheet, true, true, Resources.spritesheets().getCustomKeyFrameDurations(spritesheet.getName()));
     }
 
-    if (!this.getEntity().isAddShadow()) {
-      return currentImage;
+    @Nullable
+    private static Spritesheet findSpriteSheet(final Prop prop, final PropState state) {
+        if (prop == null || prop.getSpritesheetName() == null || prop.getSpritesheetName().isEmpty()) {
+            return null;
+        }
+        Spritesheet opt = Resources.spritesheets().get(getSpriteName(prop, state, true));
+        if (opt != null) {
+            return opt;
+        }
+        return Resources.spritesheets().get(getSpriteName(prop, state, false));
     }
-
-    // add a shadow at the lower end of the current sprite.
-    final int ShadowYOffset = currentImage.getHeight();
-    final BufferedImage shadow = Imaging.addShadow(currentImage, 0, ShadowYOffset);
-    Resources.images().add(cacheKey, shadow);
-
-    return shadow;
-  }
-
-  @Override
-  public void update() {
-    super.update();
-    this.play(this.getEntity().getState().spriteString());
-  }
-
-  @Override
-  public boolean isAutoScaling() {
-    return this.getEntity().isScaling();
-  }
-
-  private static Animation createAnimation(final Prop prop, final PropState state) {
-    final Spritesheet spritesheet = findSpriteSheet(prop, state);
-    if (spritesheet == null) {
-      return null;
-    }
-
-    return new Animation(state.spriteString(), spritesheet, true, true, Resources.spritesheets().getCustomKeyFrameDurations(spritesheet.getName()));
-  }
-
-  private static Spritesheet findSpriteSheet(final Prop prop, final PropState state) {
-    if (prop == null || prop.getSpritesheetName() == null || prop.getSpritesheetName().isEmpty()) {
-      return null;
-    }
-
-    Spritesheet opt = Resources.spritesheets().get(getSpriteName(prop, state, true));
-
-    if (opt != null) {
-      return opt;
-    }
-
-    return Resources.spritesheets().get(getSpriteName(prop, state, false));
-  }
 }
--- src/de/gurkenlabs/litiengine/gui/screens/ScreenManager.java
+++ src/de/gurkenlabs/litiengine/gui/screens/ScreenManager.java
@@ -1,12 +1,12 @@
 package de.gurkenlabs.litiengine.gui.screens;
 
+import javax.annotation.Nullable;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.GameWindow;
 import de.gurkenlabs.litiengine.graphics.RenderComponent;
@@ -19,207 +19,203 @@ import de.gurkenlabs.litiengine.graphics.RenderComponent;
  * Overwriting this method provides the ability to define a customized render pipeline that suits the need of a particular Screen implementation. With
  * the GameScreen, the LITIENGINE provides a simple default Screen implementation that renders the current {@code Environment} and all its
  * {@code GuiComponents}.
- * 
+ *
  * @see Screen
  * @see RenderComponent
  * @see GameScreen
  * @see Screen#render(java.awt.Graphics2D)
  */
 public final class ScreenManager {
-  private static final Logger log = Logger.getLogger(ScreenManager.class.getName());
-  private static final int DEFAULT_CHANGE_COOLDOWN = 200;
 
-  private final List<ScreenChangedListener> screenChangedListeners;
+    private static final Logger log = Logger.getLogger(ScreenManager.class.getName());
 
-  private final List<Screen> screens;
+    private static final int DEFAULT_CHANGE_COOLDOWN = 200;
 
-  private Screen currentScreen;
+    private final List<ScreenChangedListener> screenChangedListeners;
 
-  private int changeCooldown = DEFAULT_CHANGE_COOLDOWN;
-  private long lastScreenChange = 0;
+    private final List<Screen> screens;
 
-  /**
-   * <p>
-   * <b>You should never call this manually! Instead use the {@code Game.screens()} instance.</b>
-   * </p>
-   * 
-   * @see Game#screens()
-   */
-  public ScreenManager() {
-    if (Game.screens() != null) {
-      throw new UnsupportedOperationException("Never initialize a ScreenManager manually. Use Game.screens() instead.");
+    @Nullable
+    private Screen currentScreen;
+
+    private int changeCooldown = DEFAULT_CHANGE_COOLDOWN;
+
+    private long lastScreenChange = 0;
+
+    /**
+     * <p>
+     * <b>You should never call this manually! Instead use the {@code Game.screens()} instance.</b>
+     * </p>
+     *
+     * @see Game#screens()
+     */
+    public ScreenManager() {
+        if (Game.screens() != null) {
+            throw new UnsupportedOperationException("Never initialize a ScreenManager manually. Use Game.screens() instead.");
+        }
+        this.screenChangedListeners = new CopyOnWriteArrayList<>();
+        this.screens = new CopyOnWriteArrayList<>();
     }
 
-    this.screenChangedListeners = new CopyOnWriteArrayList<>();
-    this.screens = new CopyOnWriteArrayList<>();
-  }
-
-  /**
-   * Adds the specified screen changed listener to receive events when the current screen was changed.
-   * 
-   * @param listener
-   *          The listener to add.
-   */
-  public void addScreenChangedListener(ScreenChangedListener listener) {
-    this.screenChangedListeners.add(listener);
-  }
-
-  /**
-   * Removes the specified screen changed listener.
-   * 
-   * @param listener
-   *          The listener to remove.
-   */
-  public void removeScreenChangedListener(ScreenChangedListener listener) {
-    this.screenChangedListeners.remove(listener);
-  }
-
-  /**
-   * Adds the specified screen instance to the manager.
-   * 
-   * @param screen
-   *          The screen to add.
-   */
-  public void add(final Screen screen) {
-    screen.setWidth(Game.window().getWidth());
-    screen.setHeight(Game.window().getHeight());
-    this.screens.add(screen);
-
-    if (this.current() == null) {
-      this.display(screen);
-    }
-  }
-
-  /**
-   * Removes the specified screen instance from the manager.
-   * 
-   * @param screen
-   *          The screen to remove.
-   */
-  public void remove(Screen screen) {
-    this.screens.remove(screen);
-    if (this.current() == screen) {
-      if (!this.screens.isEmpty()) {
-        this.display(this.screens.get(0));
-      } else {
-        this.display((Screen) null);
-      }
-    }
-  }
-
-  /**
-   * Displays the specified screen by setting
-   * 
-   * @param screen
-   *          The screen to be displayed.
-   */
-  public void display(final Screen screen) {
-    if (Game.hasStarted() && Game.time().since(this.lastScreenChange) < this.getChangeCooldown()) {
-      log.log(Level.INFO, "Skipping displaying of screen {0} because screen changing is currently on cooldown.", screen != null ? screen.getName() : "");
-      return;
+    /**
+     * Adds the specified screen changed listener to receive events when the current screen was changed.
+     *
+     * @param listener
+     *          The listener to add.
+     */
+    public void addScreenChangedListener(ScreenChangedListener listener) {
+        this.screenChangedListeners.add(listener);
     }
 
-    final Screen previous = this.current();
-    if (previous != null) {
-      previous.suspend();
+    /**
+     * Removes the specified screen changed listener.
+     *
+     * @param listener
+     *          The listener to remove.
+     */
+    public void removeScreenChangedListener(ScreenChangedListener listener) {
+        this.screenChangedListeners.remove(listener);
     }
 
-    if (screen != null && !this.screens.contains(screen)) {
-      this.screens.add(screen);
+    /**
+     * Adds the specified screen instance to the manager.
+     *
+     * @param screen
+     *          The screen to add.
+     */
+    public void add(final Screen screen) {
+        screen.setWidth(Game.window().getWidth());
+        screen.setHeight(Game.window().getHeight());
+        this.screens.add(screen);
+        if (this.current() == null) {
+            this.display(screen);
+        }
     }
 
-    this.currentScreen = screen;
-    if (!Game.isInNoGUIMode() && this.current() != null) {
-      this.current().prepare();
+    /**
+     * Removes the specified screen instance from the manager.
+     *
+     * @param screen
+     *          The screen to remove.
+     */
+    public void remove(Screen screen) {
+        this.screens.remove(screen);
+        if (this.current() == screen) {
+            if (!this.screens.isEmpty()) {
+                this.display(this.screens.get(0));
+            } else {
+                this.display((Screen) null);
+            }
+        }
     }
 
-    this.lastScreenChange = Game.loop().getTicks();
-
-    final ScreenChangedEvent event = new ScreenChangedEvent(this.current(), previous);
-    for (final ScreenChangedListener listener : this.screenChangedListeners) {
-      listener.changed(event);
+    /**
+     * Displays the specified screen by setting
+     *
+     * @param screen
+     *          The screen to be displayed.
+     */
+    public void display(final Screen screen) {
+        if (Game.hasStarted() && Game.time().since(this.lastScreenChange) < this.getChangeCooldown()) {
+            log.log(Level.INFO, "Skipping displaying of screen {0} because screen changing is currently on cooldown.", screen != null ? screen.getName() : "");
+            return;
+        }
+        final Screen previous = this.current();
+        if (previous != null) {
+            previous.suspend();
+        }
+        if (screen != null && !this.screens.contains(screen)) {
+            this.screens.add(screen);
+        }
+        this.currentScreen = screen;
+        if (!Game.isInNoGUIMode() && this.current() != null) {
+            this.current().prepare();
+        }
+        this.lastScreenChange = Game.loop().getTicks();
+        final ScreenChangedEvent event = new ScreenChangedEvent(this.current(), previous);
+        for (final ScreenChangedListener listener : this.screenChangedListeners) {
+            listener.changed(event);
+        }
     }
-  }
 
-  /**
-   * Displays the {@code Screen} with the specified name.
-   * 
-   * @param screenName
-   *          The name of the screen to be displayed.
-   */
-  public void display(final String screenName) {
-    if (this.current() != null && this.current().getName().equalsIgnoreCase(screenName)) {
-      log.log(Level.INFO, "Skipping displaying of screen {0} because it is already the current screen.", screenName);
-      return;
+    /**
+     * Displays the {@code Screen} with the specified name.
+     *
+     * @param screenName
+     *          The name of the screen to be displayed.
+     */
+    public void display(final String screenName) {
+        if (this.current() != null && this.current().getName().equalsIgnoreCase(screenName)) {
+            log.log(Level.INFO, "Skipping displaying of screen {0} because it is already the current screen.", screenName);
+            return;
+        }
+        if (this.screens.stream().noneMatch(element -> element.getName().equalsIgnoreCase(screenName))) {
+            log.log(Level.WARNING, "Could not display the screen {0} because there is no screen with the specified name.", screenName);
+            return;
+        }
+        Screen screen = this.get(screenName);
+        if (screen == null) {
+            return;
+        }
+        this.display(screen);
     }
 
-    if (this.screens.stream().noneMatch(element -> element.getName().equalsIgnoreCase(screenName))) {
-      log.log(Level.WARNING, "Could not display the screen {0} because there is no screen with the specified name.", screenName);
-      return;
+    /**
+     * Gets the screen by its name.
+     *
+     * @param screenName
+     *          The name of the screen.
+     * @return The
+     */
+    @Nullable
+    public Screen get(String screenName) {
+        Optional<Screen> opt = this.screens.stream().filter(element -> element.getName().equalsIgnoreCase(screenName)).findFirst();
+        return opt.orElse(null);
     }
 
-    Screen screen = this.get(screenName);
-    if (screen == null) {
-      return;
+    /**
+     * Gets all screens of the game.
+     *
+     * @return All screens that have been previously added to this instance.
+     *
+     * @see #add(Screen)
+     */
+    public Collection<Screen> getAll() {
+        return this.screens;
     }
 
-    this.display(screen);
-  }
+    /**
+     * Gets the currently active screen that is being rendered by the {@code RenderComponent}.
+     *
+     * @return The currently active screen.
+     *
+     * @see GameWindow#getRenderComponent()
+     * @see RenderComponent#render()
+     */
+    @Nullable
+    public Screen current() {
+        return this.currentScreen;
+    }
 
-  /**
-   * Gets the screen by its name.
-   * 
-   * @param screenName
-   *          The name of the screen.
-   * @return The
-   */
-  public Screen get(String screenName) {
-    Optional<Screen> opt = this.screens.stream().filter(element -> element.getName().equalsIgnoreCase(screenName)).findFirst();
-    return opt.orElse(null);
-  }
+    /**
+     * Gets the screen change cooldown which is used to ensure that screens cannot be switched too quickly while the game is running.
+     *
+     * @return The current change timeout for screens.
+     *
+     * @see #DEFAULT_CHANGE_COOLDOWN
+     * @see Game#hasStarted()
+     */
+    public int getChangeCooldown() {
+        return this.changeCooldown;
+    }
 
-  /**
-   * Gets all screens of the game.
-   * 
-   * @return All screens that have been previously added to this instance.
-   * 
-   * @see #add(Screen)
-   */
-  public Collection<Screen> getAll() {
-    return this.screens;
-  }
-
-  /**
-   * Gets the currently active screen that is being rendered by the {@code RenderComponent}.
-   * 
-   * @return The currently active screen.
-   * 
-   * @see GameWindow#getRenderComponent()
-   * @see RenderComponent#render()
-   */
-  public Screen current() {
-    return this.currentScreen;
-  }
-
-  /**
-   * Gets the screen change cooldown which is used to ensure that screens cannot be switched too quickly while the game is running.
-   * 
-   * @return The current change timeout for screens.
-   * 
-   * @see #DEFAULT_CHANGE_COOLDOWN
-   * @see Game#hasStarted()
-   */
-  public int getChangeCooldown() {
-    return this.changeCooldown;
-  }
-
-  /**
-   * Sets the cooldown for changing screens.
-   * 
-   * @param changeCooldown
-   *          The cooldown for changing screens.
-   */
-  public void setChangeCooldown(int changeCooldown) {
-    this.changeCooldown = changeCooldown;
-  }
+    /**
+     * Sets the cooldown for changing screens.
+     *
+     * @param changeCooldown
+     *          The cooldown for changing screens.
+     */
+    public void setChangeCooldown(int changeCooldown) {
+        this.changeCooldown = changeCooldown;
+    }
 }
