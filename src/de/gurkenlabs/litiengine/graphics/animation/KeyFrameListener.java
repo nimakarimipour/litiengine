069a1d68ad28f4c31a3c2816bd28164e3877b0a1
nimakarimipour <karimipour.nima@gmail.com>
2022-04-09 21:36:43 -0700

    Final Result

--- src/de/gurkenlabs/litiengine/graphics/animation/KeyFrameListener.java
+++ src/de/gurkenlabs/litiengine/graphics/animation/KeyFrameListener.java
@@ -1,7 +1,9 @@
 package de.gurkenlabs.litiengine.graphics.animation;
 
+import javax.annotation.Nullable;
 import java.util.EventListener;
 
 public interface KeyFrameListener extends EventListener {
-  public void currentFrameChanged(KeyFrame previousFrame, KeyFrame currentFrame);
+
+    public void currentFrameChanged(KeyFrame previousFrame, KeyFrame currentFrame);
 }
