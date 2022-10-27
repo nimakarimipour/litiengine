7428b6180b1c681e8fcf5cb266b4e8a2991d7c95
nimakarimipour <karimipour.nima@gmail.com>
2022-10-26 18:33:47 -0700

    update

--- src/de/gurkenlabs/litiengine/graphics/RotationImageEffect.java
+++ src/de/gurkenlabs/litiengine/graphics/RotationImageEffect.java
@@ -1,43 +1,43 @@
 package de.gurkenlabs.litiengine.graphics;
 
+import javax.annotation.Nullable;
 import java.awt.Graphics2D;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
-
 import de.gurkenlabs.litiengine.util.Imaging;
 
 public class RotationImageEffect extends ImageEffect {
-  private final float angle;
 
-  /**
-   * Initializes a new instance of the {@code RotationImageEffect}.
-   *
-   * @param ttl
-   *          The time to live of this effect.
-   * @param angle
-   *          The angle by which this effect rotates the base image.
-   */
-  public RotationImageEffect(final int ttl, final float angle) {
-    super(ttl, "RotationImageEffect");
-    this.angle = angle;
-  }
+    private final float angle;
 
-  @Override
-  public BufferedImage apply(final BufferedImage image) {
-    if (image == null) {
-      return null;
+    /**
+     * Initializes a new instance of the {@code RotationImageEffect}.
+     *
+     * @param ttl
+     *          The time to live of this effect.
+     * @param angle
+     *          The angle by which this effect rotates the base image.
+     */
+    public RotationImageEffect(final int ttl, final float angle) {
+        super(ttl, "RotationImageEffect");
+        this.angle = angle;
     }
 
-    final int size = Math.max(image.getWidth(), image.getHeight()) * 2;
-    final BufferedImage img = Imaging.getCompatibleImage(size, size);
-    final Graphics2D g = img.createGraphics();
-    ImageRenderer.renderRotated(g, image, new Point2D.Double(0, 0), this.getAngle());
-    g.dispose();
+    @Override
+    @Nullable
+    public BufferedImage apply(final BufferedImage image) {
+        if (image == null) {
+            return null;
+        }
+        final int size = Math.max(image.getWidth(), image.getHeight()) * 2;
+        final BufferedImage img = Imaging.getCompatibleImage(size, size);
+        final Graphics2D g = img.createGraphics();
+        ImageRenderer.renderRotated(g, image, new Point2D.Double(0, 0), this.getAngle());
+        g.dispose();
+        return img;
+    }
 
-    return img;
-  }
-
-  public double getAngle() {
-    return this.angle;
-  }
+    public double getAngle() {
+        return this.angle;
+    }
 }
