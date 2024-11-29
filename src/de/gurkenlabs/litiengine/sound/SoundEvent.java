411de9cdf96e1467f1cf39fc88a3f082488d8744
Nima Karimipour <karimipour.nima@gmail.com>
2024-11-29 12:46:26 -0800

    resolve

--- src/de/gurkenlabs/litiengine/sound/SoundEvent.java
+++ src/de/gurkenlabs/litiengine/sound/SoundEvent.java
@@ -30,6 +30,6 @@ public class SoundEvent extends EventObject {
 
   @Override
   public String toString() {
-    return super.toString() + "[sound=" + this.sound.getName() + "]";
+    return super.toString() + "[sound=" + (this.sound == null ? "null" : this.sound.getName()) + "]";
   }
 }
