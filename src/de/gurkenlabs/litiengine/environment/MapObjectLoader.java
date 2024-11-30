cae96411d8214f1fb7503e4269b5c8a7e5d75172
Nima Karimipour <karimipour.nima@gmail.com>
2024-11-29 21:24:42 -0800

    resolve

--- src/de/gurkenlabs/litiengine/environment/MapObjectLoader.java
+++ src/de/gurkenlabs/litiengine/environment/MapObjectLoader.java
@@ -93,7 +93,7 @@ public abstract class MapObjectLoader implements IMapObjectLoader {
   }
 
   protected boolean isMatchingType(IMapObject mapObject) {
-    if (!mapObject.getType().equalsIgnoreCase(this.getMapObjectType())) {
+    if (mapObject.getType() == null || !mapObject.getType().equalsIgnoreCase(this.getMapObjectType())) {
       log.log(Level.SEVERE, "Cannot load a mapobject of the type [{0}] with a loader of type [{1}].", new Object[]{mapObject.getType(), this.getClass()});
       return false;
     }
