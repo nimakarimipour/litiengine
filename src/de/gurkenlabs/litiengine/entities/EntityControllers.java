91ab2eb01db3c5a9620087e84b0f11db742a70f4
Nima Karimipour <karimipour.nima@gmail.com>
2025-03-31 00:50:07 -0700

    Revert "fix: 78 - METHOD_NO_INIT - public SoundSource(String name) { - initializer method does not guarantee @NonNull fields sound (line 29), playback (line 30) are initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 0a442ce70dbfc92b797e436330e26b644c829fbc.

--- src/de/gurkenlabs/litiengine/entities/EntityControllers.java
+++ src/de/gurkenlabs/litiengine/entities/EntityControllers.java
@@ -18,8 +18,7 @@ public final class EntityControllers {
     this.controllers = new ConcurrentHashMap<>();
   }
 
-  @Nullable
-  public IEntityAnimationController getAnimationController() {
+  @Nullable public IEntityAnimationController getAnimationController() {
     if (this.animationController == null) {
       this.animationController = this.getController(IEntityAnimationController.class);
     }
@@ -27,8 +26,7 @@ public final class EntityControllers {
     return this.animationController;
   }
 
-  @Nullable
-  @SuppressWarnings("unchecked")
+  @Nullable @SuppressWarnings("unchecked")
   public <T extends IEntityController> T getController(Class<T> clss) {
     T explicitController = this.getExplicitController(clss);
     if (explicitController != null) {
@@ -84,8 +82,7 @@ public final class EntityControllers {
     }
   }
 
-  @Nullable
-  @SuppressWarnings("unchecked")
+  @Nullable @SuppressWarnings("unchecked")
   private <T extends IEntityController> T getExplicitController(Class<T> clss) {
     // if there's an exact match, return it
     if (this.controllers.containsKey(clss)) {
--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/BooleanIntegerAdapter.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/BooleanIntegerAdapter.java
@@ -1,17 +1,15 @@
 package de.gurkenlabs.litiengine.environment.tilemap.xml;
 
-import javax.annotation.Nullable;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
+import javax.annotation.Nullable;
 
 public class BooleanIntegerAdapter extends XmlAdapter<Integer, Boolean> {
-  @Nullable
-  @Override
+  @Nullable @Override
   public Boolean unmarshal(Integer s) {
     return s == null ? null : s == 1;
   }
 
-  @Nullable
-  @Override
+  @Nullable @Override
   public Integer marshal(Boolean c) {
     if (c == null) {
       return null;
