c32f676937ab91c2ec7dc08a821ce8f55f42d2bd
Nima Karimipour <karimipour.nima@gmail.com>
2025-03-31 00:45:27 -0700

    Revert "fix: 47 - RETURN_NULLABLE - return this.camera; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 816d54ec1bd2ecf1f59aef3bed79c22f6e8d0c54.

--- src/de/gurkenlabs/litiengine/environment/GameWorld.java
+++ src/de/gurkenlabs/litiengine/environment/GameWorld.java
@@ -227,7 +227,6 @@ public final class GameWorld implements IUpdateable {
    * @return The currently active camera.
    * @see ICamera
    */
-  @Nullable
   public ICamera camera() {
     return this.camera;
   }
@@ -287,8 +286,7 @@ public final class GameWorld implements IUpdateable {
    * @param mapName The map name by which the environment is identified.
    * @return The environment for the map name or null if no such map can be found.
    */
-  @Nullable
-  public Environment getEnvironment(String mapName) {
+  @Nullable public Environment getEnvironment(String mapName) {
     if (mapName == null || mapName.isEmpty()) {
       return null;
     }
@@ -304,8 +302,7 @@ public final class GameWorld implements IUpdateable {
    * @param map The map by which the environment is identified.
    * @return The environment for the map or null if no such map can be found.
    */
-  @Nullable
-  public Environment getEnvironment(IMap map) {
+  @Nullable public Environment getEnvironment(IMap map) {
     if (map == null || map.getName() == null || map.getName().isEmpty()) {
       return null;
     }
@@ -395,8 +392,7 @@ public final class GameWorld implements IUpdateable {
    * @see GameWorld#environment()
    * @see GameWorld#loadEnvironment(Environment)
    */
-  @Nullable
-  public Environment loadEnvironment(String mapName) {
+  @Nullable public Environment loadEnvironment(String mapName) {
     Environment env = this.getEnvironment(mapName);
     this.loadEnvironment(env);
     return env;
@@ -414,8 +410,7 @@ public final class GameWorld implements IUpdateable {
    * @see GameWorld#environment()
    * @see GameWorld#loadEnvironment(Environment)
    */
-  @Nullable
-  public Environment loadEnvironment(IMap map) {
+  @Nullable public Environment loadEnvironment(IMap map) {
     Environment env = this.getEnvironment(map);
     this.loadEnvironment(env);
     return env;
@@ -456,8 +451,7 @@ public final class GameWorld implements IUpdateable {
    * @see GameWorld#getEnvironment(String)
    * @see GameWorld#reset(IMap)
    */
-  @Nullable
-  public Environment reset(String mapName) {
+  @Nullable public Environment reset(String mapName) {
     if (mapName == null || mapName.isEmpty()) {
       return null;
     }
@@ -478,8 +472,7 @@ public final class GameWorld implements IUpdateable {
    * @see GameWorld#getEnvironment(String)
    * @see GameWorld#reset(IMap)
    */
-  @Nullable
-  public Environment reset(IMap map) {
+  @Nullable public Environment reset(IMap map) {
     if (map == null) {
       return null;
     }
@@ -563,8 +556,7 @@ public final class GameWorld implements IUpdateable {
     listeners.get(mapIdentifier).remove(listener);
   }
 
-  @Nullable
-  private static String getMapName(Environment env) {
+  @Nullable private static String getMapName(Environment env) {
     if (env.getMap() != null && env.getMap().getName() != null) {
       return env.getMap().getName().toLowerCase();
     }
