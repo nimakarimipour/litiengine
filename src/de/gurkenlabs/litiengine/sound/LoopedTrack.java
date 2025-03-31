f53aa5366265ec77ac9bf4305e2602204c39f2a8
Nima Karimipour <karimipour.nima@gmail.com>
2025-03-31 01:02:13 -0700

    Revert "fix: 130 - DEREFERENCE_NULLABLE - return Collections.unmodifiableCollection(this.miscEntities.get(renderType).values()); - dereferenced expression this.miscEntities.get(renderType) is @Nullable"
    This reverts commit f2eb73ad6a0a5cf558739f12d74a5ef41678ae77.

--- src/de/gurkenlabs/litiengine/sound/LoopedTrack.java
+++ src/de/gurkenlabs/litiengine/sound/LoopedTrack.java
@@ -3,8 +3,8 @@ package de.gurkenlabs.litiengine.sound;
 import de.gurkenlabs.litiengine.resources.Resources;
 import java.util.Iterator;
 import java.util.Objects;
-import javax.annotation.Nullable;
 import javax.sound.sampled.AudioFormat;
+import javax.annotation.Nullable;
 
 public class LoopedTrack implements Track, Iterator<Sound> {
   private Sound track;
@@ -32,8 +32,7 @@ public class LoopedTrack implements Track, Iterator<Sound> {
     return this;
   }
 
-  @Nullable
-  @Override
+  @Nullable @Override
   public AudioFormat getFormat() {
     return this.track.getFormat();
   }
