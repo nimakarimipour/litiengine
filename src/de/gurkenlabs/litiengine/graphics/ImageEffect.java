efefc7981d918c94ae5e92f593fc06b5f9122e40
Nima Karimipour <karimipour.nima@gmail.com>
2025-03-31 00:55:12 -0700

    fix: 101 - DEREFERENCE_NULLABLE - layer.addMapObject(mapObject); - dereferenced expression layer is @Nullable

--- src/de/gurkenlabs/litiengine/graphics/ImageEffect.java
+++ src/de/gurkenlabs/litiengine/graphics/ImageEffect.java
@@ -58,5 +58,6 @@ public abstract class ImageEffect implements ITimeToLive, Comparable<ImageEffect
     return Integer.compare(this.getPriority(), other.getPriority());
   }
 
-  @Nullable public abstract BufferedImage apply(BufferedImage image);
+  @Nullable
+  public abstract BufferedImage apply(BufferedImage image);
 }
