a08312c80310668d0cc69fb12a0dfb41607bde38
Nima Karimipour <karimipour.nima@gmail.com>
2025-03-31 00:48:16 -0700

    Revert "fix: 69 - DEREFERENCE_NULLABLE - this.collisionEntities.get(type).stream() - dereferenced expression this.collisionEntities.get(type) is @Nullable"
    This reverts commit 882f09a9aaf5102782d32ec8ac9fab5e2764e069.

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/Terrain.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/Terrain.java
@@ -1,10 +1,10 @@
 package de.gurkenlabs.litiengine.environment.tilemap.xml;
 
 import de.gurkenlabs.litiengine.environment.tilemap.ITerrain;
-import javax.annotation.Nullable;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.annotation.Nullable;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Terrain extends CustomPropertyProvider implements ITerrain {
@@ -14,8 +14,7 @@ public class Terrain extends CustomPropertyProvider implements ITerrain {
 
   @XmlAttribute private int tile;
 
-  @Nullable
-  @Override
+  @Nullable @Override
   public String getName() {
     return this.name;
   }
