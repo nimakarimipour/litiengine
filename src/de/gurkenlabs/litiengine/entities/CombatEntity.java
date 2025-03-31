943df39813c4796896c9a84b410026113f74440c
Nima Karimipour <karimipour.nima@gmail.com>
2025-03-31 01:01:56 -0700

    fix: 128 - UNBOX_NULLABLE - return this.y - map.getChunkOffsetY() * map.getTileHeight(); - unboxing of a @Nullable value

--- src/de/gurkenlabs/litiengine/entities/CombatEntity.java
+++ src/de/gurkenlabs/litiengine/entities/CombatEntity.java
@@ -140,7 +140,8 @@ public class CombatEntity extends CollisionEntity implements ICombatEntity {
     return new Ellipse2D.Double(this.getX(), this.getY(), this.getWidth(), this.getHeight());
   }
 
-  @Nullable @Override
+  @Nullable
+  @Override
   public ICombatEntity getTarget() {
     return this.target;
   }
