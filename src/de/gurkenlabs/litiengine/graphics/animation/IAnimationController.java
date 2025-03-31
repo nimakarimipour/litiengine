7074d10e5c61514debe0ff23b2af28856b72b206
Nima Karimipour <karimipour.nima@gmail.com>
2025-03-31 00:50:34 -0700

    fix: 81 - DEREFERENCE_NULLABLE - Graphics2D g = debugCursorImg.createGraphics(); - dereferenced expression debugCursorImg is @Nullable

--- src/de/gurkenlabs/litiengine/graphics/animation/IAnimationController.java
+++ src/de/gurkenlabs/litiengine/graphics/animation/IAnimationController.java
@@ -71,7 +71,8 @@ public interface IAnimationController extends IUpdateable {
    * @see #getDefault()
    * @see #hasAnimation(String)
    */
-  @Nullable public Animation get(String animationName);
+  @Nullable
+  public Animation get(String animationName);
 
   /**
    * Gets the currently active {@code Animation} of this controller.
@@ -84,7 +85,8 @@ public interface IAnimationController extends IUpdateable {
    * @see #get(String)
    * @see RenderEngine#renderEntity(java.awt.Graphics2D, de.gurkenlabs.litiengine.entities.IEntity)
    */
-  @Nullable public Animation getCurrent();
+  @Nullable
+  public Animation getCurrent();
 
   /**
    * Gets the default {@code Animation} of this controller.
@@ -96,7 +98,8 @@ public interface IAnimationController extends IUpdateable {
    * @see #get(String)
    * @see #setDefault(Animation)
    */
-  @Nullable public Animation getDefault();
+  @Nullable
+  public Animation getDefault();
 
   /**
    * Determines whether this controller has an {@code Animation} with the specified name.
@@ -158,7 +161,8 @@ public interface IAnimationController extends IUpdateable {
    * @see Animation#getCurrentKeyFrame()
    * @see #isEnabled()
    */
-  @Nullable public BufferedImage getCurrentImage();
+  @Nullable
+  public BufferedImage getCurrentImage();
 
   /**
    * Gets the current sprite scaled by the specified dimensions of the currently active animation of
@@ -175,7 +179,8 @@ public interface IAnimationController extends IUpdateable {
    * @see Animation#getCurrentKeyFrame()
    * @see #isEnabled()
    */
-  @Nullable public BufferedImage getCurrentImage(int width, int height);
+  @Nullable
+  public BufferedImage getCurrentImage(int width, int height);
 
   /**
    * Gets the {@code AffineTransform} instance assigned to this controller that can be used to
@@ -187,7 +192,8 @@ public interface IAnimationController extends IUpdateable {
    * @see #setAffineTransform(AffineTransform)
    * @see #getCurrentImage()
    */
-  @Nullable public AffineTransform getAffineTransform();
+  @Nullable
+  public AffineTransform getAffineTransform();
 
   /**
    * Sets the {@code AffineTransform} instance for this controller that can be used to externally
