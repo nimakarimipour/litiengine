f2e9ef2174ff7235e27230d979f389e9960ea01a
Nima Karimipour <karimipour.nima@gmail.com>
2025-03-31 00:42:47 -0700

    Revert "fix: 32 - DEREFERENCE_NULLABLE - this.onChange(e -> this.sliderComponent.setLocation(this.getRelativeSliderPosition())); - dereferenced expression this.sliderComponent is @Nullable"
    This reverts commit 0a8e43fa8eb70d31303a19f5e09d5df5eb1dbf54.

--- src/de/gurkenlabs/litiengine/entities/Trigger.java
+++ src/de/gurkenlabs/litiengine/entities/Trigger.java
@@ -53,8 +53,7 @@ public class Trigger extends CollisionEntity implements IUpdateable {
   @TmxProperty(name = MapObjectProperty.TRIGGER_ONETIME)
   private final boolean isOneTimeTrigger;
 
-  @Nullable
-  @TmxProperty(name = MapObjectProperty.TRIGGER_MESSAGE)
+  @Nullable @TmxProperty(name = MapObjectProperty.TRIGGER_MESSAGE)
   private String message;
 
   @TmxProperty(name = MapObjectProperty.TRIGGER_COOLDOWN)
@@ -80,8 +79,7 @@ public class Trigger extends CollisionEntity implements IUpdateable {
    * @param name The name of this trigger.
    * @param message The message that gets sent by this trigger upon activation.
    */
-  public Trigger(
-      final TriggerActivation activation, @Nullable final String name, final String message) {
+  public Trigger(final TriggerActivation activation, @Nullable final String name, final String message) {
     this(activation, name, message, false);
   }
 
@@ -195,8 +193,7 @@ public class Trigger extends CollisionEntity implements IUpdateable {
     return this.activators;
   }
 
-  @Nullable
-  public String getMessage() {
+  @Nullable public String getMessage() {
     return this.message;
   }
 
