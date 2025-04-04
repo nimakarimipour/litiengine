165d09665c939156661d8811b1f1696953291d6e
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-04 14:22:29 -0700

    Revert "fix: 102 - DEREFERENCE_NULLABLE - this.getMapObjectLayers().stream().filter(x -> x.getName().equals(layerName)).findFirst(); - dereferenced expression x.getName() is @Nullable"
    This reverts commit 7f0a61e8400dcca5220667f4d3b828a55e26c3c8.

--- src/de/gurkenlabs/litiengine/environment/tilemap/ILayerList.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/ILayerList.java
@@ -68,9 +68,7 @@ public interface ILayerList extends ICustomPropertyProvider {
   @Nullable
   public default IMapObjectLayer getMapObjectLayer(String layerName) {
     Optional<IMapObjectLayer> layer =
-        this.getMapObjectLayers().stream()
-            .filter(x -> x.getName() != null && x.getName().equals(layerName))
-            .findFirst();
+        this.getMapObjectLayers().stream().filter(x -> x.getName().equals(layerName)).findFirst();
     return layer.orElse(null);
   }
 
