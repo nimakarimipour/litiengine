0e7a28c5ee2d02b2ca3dd8730834078a0c1fe272
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-04 14:33:56 -0700

    Revert "fix: 34 - RETURN_NULLABLE - return this.color; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit e19188c7d7f91d839dd1ec81cc28f177cad43ad0.

--- src/de/gurkenlabs/litiengine/graphics/emitters/particles/TextParticle.java
+++ src/de/gurkenlabs/litiengine/graphics/emitters/particles/TextParticle.java
@@ -51,21 +51,18 @@ public class TextParticle extends Particle {
     g.setFont(g.getFont().deriveFont(rotate));
 
     RenderingHints originalHints = g.getRenderingHints();
-    Color color = this.getColor();
-    if (color != null) {
-      g.setColor(
-          new Color(
-              color.getRed() / 255f,
-              color.getGreen() / 255f,
-              color.getBlue() / 255f,
-              this.getOpacity()));
-    }
+    g.setColor(
+        new Color(
+            this.getColor().getRed() / 255f,
+            this.getColor().getGreen() / 255f,
+            this.getColor().getBlue() / 255f,
+            this.getOpacity()));
 
     if (this.isAntiAliased()) {
       TextRenderer.enableTextAntiAliasing(g);
     }
 
-    g.drawString(this.getText(), (float) renderLocation.getX(), (float) renderLocation.getY());
+    g.drawString(text, (float) renderLocation.getX(), (float) renderLocation.getY());
     g.setFont(oldFont);
     g.setRenderingHints(originalHints);
   }
