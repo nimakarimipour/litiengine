aa1d80aef3c1a2bed60b2d3540d5af3878e5b6b0
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-04 14:55:53 -0700

    Revert "fix: 37 - DEREFERENCE_NULLABLE - while (measurer.getPosition() < this.currentText.length()) { - dereferenced expression this.currentText is @Nullable"
    This reverts commit 7f0812dd8a657db80a767c13f557b0f28a74341d.

--- src/de/gurkenlabs/litiengine/gui/SpeechBubble.java
+++ src/de/gurkenlabs/litiengine/gui/SpeechBubble.java
@@ -156,8 +156,6 @@ public class SpeechBubble implements IUpdateable, IRenderable {
       return;
     }
 
-    this.entityCenter = Game.world().camera().getViewportLocation(this.getEntity().getCenter());
-
     final float deltaX = (float) (this.textBoxWidth / 2.0 + this.getAppearance().getPadding());
     final float deltaY =
         (float)
@@ -256,10 +254,6 @@ public class SpeechBubble implements IUpdateable, IRenderable {
   }
 
   private void createBubbleImage() {
-    if (this.currentText == null) {
-      return;
-    }
-
     final BufferedImage img = Imaging.getCompatibleImage(500, 500);
     final Graphics2D g = img.createGraphics();
     g.setFont(this.getFont());
