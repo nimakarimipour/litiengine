380e15a230e5754fbf0223112adcb9afe298715a
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-04 13:52:22 -0700

    fix: 66 - RETURN_NULLABLE - return this.frames; - returning @Nullable expression from method with @NonNull return type

--- src/de/gurkenlabs/litiengine/environment/tilemap/ITileAnimation.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/ITileAnimation.java
@@ -1,8 +1,10 @@
 package de.gurkenlabs.litiengine.environment.tilemap;
 
 import java.util.List;
+import javax.annotation.Nullable;
 
 public interface ITileAnimation {
+  @Nullable
   public List<ITileAnimationFrame> getFrames();
 
   public int getTotalDuration();
