535bec6c4f4c3109fa08214bc407adbd0457f6ce
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-04 14:23:22 -0700

    Revert "fix: 113 - RETURN_NULLABLE - return this.environment; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit eb608a759875aa2613c6faa8f281739b15388d0b.

--- src/de/gurkenlabs/litiengine/entities/Trigger.java
+++ src/de/gurkenlabs/litiengine/entities/Trigger.java
@@ -334,18 +334,9 @@ public class Trigger extends CollisionEntity implements IUpdateable {
       this.collisionActivated.add(activator);
     }
 
-    Environment env = this.getEnvironment();
-    if (env == null) {
-      log.log(
-          Level.WARNING,
-          "Environment is null for trigger [{0}]. Activation cannot proceed.",
-          new Object[] {this});
-      return false;
-    }
-
     // if we actually have a trigger target, we send the message to the target
     for (final int target : triggerTargets) {
-      final IEntity entity = env.get(target);
+      final IEntity entity = this.getEnvironment().get(target);
       if (entity == null) {
         log.log(
             Level.WARNING,
@@ -363,7 +354,7 @@ public class Trigger extends CollisionEntity implements IUpdateable {
     }
 
     if (this.isOneTimeTrigger) {
-      env.remove(this);
+      this.getEnvironment().remove(this);
     }
 
     this.lastActivation = Game.time().now();
