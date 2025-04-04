be7462112cec7f566b2929d1e0cc73b7e556e7bf
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-04 14:17:39 -0700

    fix: 49 - RETURN_NULLABLE - return this.resolution; - returning @Nullable expression from method with @NonNull return type

--- src/de/gurkenlabs/litiengine/graphics/FreeFlightCamera.java
+++ src/de/gurkenlabs/litiengine/graphics/FreeFlightCamera.java
@@ -80,17 +80,23 @@ public class FreeFlightCamera extends Camera implements IUpdateable {
     double deltaX = 0;
     if (mouseLocation.getX() < this.getScrollPadding()) {
       deltaX -= scrollSpeed;
-    } else if (Game.window().getResolution().getWidth() - mouseLocation.getX()
-        < this.getScrollPadding()) {
-      deltaX += scrollSpeed;
+    } else {
+      Dimension resolution = Game.window().getResolution();
+      if (resolution != null
+          && resolution.getWidth() - mouseLocation.getX() < this.getScrollPadding()) {
+        deltaX += scrollSpeed;
+      }
     }
 
     double deltaY = 0;
     if (mouseLocation.getY() < this.getScrollPadding()) {
       deltaY -= scrollSpeed;
-    } else if (Game.window().getResolution().getHeight() - mouseLocation.getY()
-        < this.getScrollPadding()) {
-      deltaY += scrollSpeed;
+    } else {
+      Dimension resolution = Game.window().getResolution();
+      if (resolution != null
+          && resolution.getHeight() - mouseLocation.getY() < this.getScrollPadding()) {
+        deltaY += scrollSpeed;
+      }
     }
 
     x += deltaX;
