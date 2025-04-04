8695629b55e47875521dd966ada42627d85cfa74
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-04 13:45:13 -0700

    Revert "fix: 23 - RETURN_NULLABLE - return this.name; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit dafef836832bd05b32178af25f98418ca3679919.

--- src/de/gurkenlabs/litiengine/gui/screens/ScreenManager.java
+++ src/de/gurkenlabs/litiengine/gui/screens/ScreenManager.java
@@ -5,6 +5,7 @@ import de.gurkenlabs.litiengine.GameWindow;
 import de.gurkenlabs.litiengine.graphics.RenderComponent;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -153,11 +154,7 @@ public final class ScreenManager {
     }
 
     if (this.screens.stream()
-        .noneMatch(
-            element ->
-                element != null
-                    && element.getName() != null
-                    && element.getName().equalsIgnoreCase(screenName))) {
+        .noneMatch(element -> element.getName().equalsIgnoreCase(screenName))) {
       log.log(
           Level.WARNING,
           "Could not display the screen {0} because there is no screen with the specified name.",
@@ -181,14 +178,11 @@ public final class ScreenManager {
    */
   @Nullable
   public Screen get(String screenName) {
-    return this.screens.stream()
-        .filter(
-            element -> {
-              String name = element.getName();
-              return name != null && name.equalsIgnoreCase(screenName);
-            })
-        .findFirst()
-        .orElse(null);
+    Optional<Screen> opt =
+        this.screens.stream()
+            .filter(element -> element.getName().equalsIgnoreCase(screenName))
+            .findFirst();
+    return opt.orElse(null);
   }
 
   /**
