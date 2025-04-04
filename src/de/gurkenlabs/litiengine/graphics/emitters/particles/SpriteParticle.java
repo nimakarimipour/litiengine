7fb888c42bde0510ea338d2ef4a75a114ca10836
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-04 14:13:50 -0700

    Revert "fix: 5 - DEREFERENCE_NULLABLE - this.animation.getDefault().setLooping(loopSprite); - dereferenced expression this.animation.getDefault() is @Nullable"
    This reverts commit 176807ebb4270e9602d560b8566b9d7f2e54c244.

--- src/de/gurkenlabs/litiengine/graphics/emitters/particles/SpriteParticle.java
+++ src/de/gurkenlabs/litiengine/graphics/emitters/particles/SpriteParticle.java
@@ -77,9 +77,6 @@ public class SpriteParticle extends Particle {
 
   public void setLoopSprite(boolean loopSprite) {
     this.loopSprite = loopSprite;
-    Animation defaultAnimation = this.animation.getDefault();
-    if (defaultAnimation != null) {
-      defaultAnimation.setLooping(loopSprite);
-    }
+    this.animation.getDefault().setLooping(loopSprite);
   }
 }
