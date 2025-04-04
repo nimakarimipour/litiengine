67f35401e0ac8e9c5976b1a1ea5674c152e72bf6
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-04 14:22:14 -0700

    Revert "fix: 99 - DEREFERENCE_NULLABLE - x.getName().equals(sprite.getName()) && x.getImage().equals(sprite.getImage()))) { - dereferenced expression x.getName() is @Nullable"
    This reverts commit fd2d4a86cb124613b7ac1b731c3af6e485672b43.

--- src/de/gurkenlabs/litiengine/resources/ResourceBundle.java
+++ src/de/gurkenlabs/litiengine/resources/ResourceBundle.java
@@ -198,15 +198,10 @@ public class ResourceBundle implements Serializable {
   void beforeMarshal(Marshaller m) {
     List<SpritesheetResource> distinctList = new ArrayList<>();
     for (SpritesheetResource sprite : this.getSpriteSheets()) {
-      if (sprite.getName() != null
-          && sprite.getImage() != null
-          && distinctList.stream()
-              .anyMatch(
-                  x ->
-                      x.getName() != null
-                          && x.getImage() != null
-                          && x.getName().equals(sprite.getName())
-                          && x.getImage().equals(sprite.getImage()))) {
+      if (distinctList.stream()
+          .anyMatch(
+              x ->
+                  x.getName().equals(sprite.getName()) && x.getImage().equals(sprite.getImage()))) {
         continue;
       }
 
@@ -217,9 +212,7 @@ public class ResourceBundle implements Serializable {
 
     List<Tileset> distinctTilesets = new ArrayList<>();
     for (Tileset tileset : this.getTilesets()) {
-      if (tileset.getName() != null
-          && distinctTilesets.stream()
-              .anyMatch(x -> x.getName() != null && x.getName().equals(tileset.getName()))) {
+      if (distinctTilesets.stream().anyMatch(x -> x.getName().equals(tileset.getName()))) {
         continue;
       }
 
