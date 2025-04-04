38e1b402585941b6c57280d8c6e1e05bc68e274f
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-04 14:18:48 -0700

    Revert "fix: 64 - METHOD_NO_INIT - @Initializer - initializer method does not guarantee @NonNull fields encoding (line 59), chunks (line 69) are initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit eece8a2f916b3f6907f7ded1a398fdb98d003dd7.

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/TileData.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/TileData.java
@@ -3,7 +3,6 @@ package de.gurkenlabs.litiengine.environment.tilemap.xml;
 import com.uber.nullaway.annotations.Initializer;
 import de.gurkenlabs.litiengine.util.ArrayUtilities;
 import de.gurkenlabs.litiengine.util.io.Codec;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -57,7 +56,7 @@ public class TileData {
     }
   }
 
-  @Nullable @XmlAttribute private String encoding;
+  @XmlAttribute private String encoding;
 
   @Nullable @XmlAttribute private String compression;
 
@@ -67,7 +66,7 @@ public class TileData {
 
   @Nullable @XmlTransient private String value;
 
-  @Nullable @XmlTransient private List<TileChunk> chunks;
+  @XmlTransient private List<TileChunk> chunks;
 
   @Nullable @XmlTransient private List<Tile> tiles;
 
@@ -120,7 +119,6 @@ public class TileData {
     this.height = height;
   }
 
-  @Nullable
   @XmlTransient
   public String getEncoding() {
     return this.encoding;
@@ -418,10 +416,6 @@ public class TileData {
 
   /** For infinite maps, the size of a tile layer depends on the {@code TileChunks} it contains. */
   private void updateDimensionsByTileData() {
-    if (this.chunks == null) {
-      return;
-    }
-
     int minX = 0;
     int maxX = 0;
     int minY = 0;
@@ -459,10 +453,6 @@ public class TileData {
   private List<Tile> parseChunkData() throws InvalidTileLayerException {
     // first fill a two-dimensional array with all the information of the chunks
     Tile[][] tileArr = new Tile[this.getHeight()][this.getWidth()];
-
-    if (this.chunks == null || this.chunks.isEmpty()) {
-      throw new InvalidTileLayerException("No tile chunks available for processing.");
-    }
 
     if (this.getEncoding().equals(Encoding.BASE64)) {
       for (TileChunk chunk : this.chunks) {
@@ -506,7 +496,7 @@ public class TileData {
 
   private List<Tile> parseData() throws InvalidTileLayerException {
     List<Tile> tmpTiles;
-    if (Nullability.castToNonnull(this.getEncoding(), "reason...").equals(Encoding.BASE64)) {
+    if (this.getEncoding().equals(Encoding.BASE64)) {
       tmpTiles = parseBase64Data(this.value, this.compression);
     } else if (this.getEncoding().equals(Encoding.CSV)) {
       tmpTiles = parseCsvData(this.value);
