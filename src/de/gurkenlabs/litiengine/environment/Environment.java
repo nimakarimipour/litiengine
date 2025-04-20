ccf4e2d18fead729922df3f89307c9ae151e5835
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-19 23:00:35 -0700

    Revert "fix: 68 - DEREFERENCE_NULLABLE - this.renderListeners.get(renderType).add(listener); - dereferenced expression this.renderListeners.get(renderType) is @Nullable"
    This reverts commit ab821decaea0b2681ef6fdabdc94b6f9ee0ee87a.

--- src/de/gurkenlabs/litiengine/environment/Environment.java
+++ src/de/gurkenlabs/litiengine/environment/Environment.java
@@ -258,12 +258,7 @@ public final class Environment implements IRenderable {
    * @param listener The listener to add.
    */
   public void onRendered(RenderType renderType, EnvironmentRenderedListener listener) {
-    Collection<EnvironmentRenderedListener> listeners = this.renderListeners.get(renderType);
-    if (listeners == null) {
-      listeners = ConcurrentHashMap.newKeySet();
-      this.renderListeners.put(renderType, listeners);
-    }
-    listeners.add(listener);
+    this.renderListeners.get(renderType).add(listener);
   }
 
   /**
