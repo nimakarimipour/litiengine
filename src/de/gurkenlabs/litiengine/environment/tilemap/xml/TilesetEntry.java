096af1604ddd74db0f8e4ade3b3fc7d93b741ad9
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-19 22:55:47 -0700

    Revert "fix: 11 - DEREFERENCE_NULLABLE - return this.tileset.getTile(this.animation.getCurrentFrame().getTileId()).getBasicImage(); - dereferenced expression this.tileset.getTile(this.animation.getCurrentFrame().getTileId()) is @Nullable"
    This reverts commit 3ea94aacead09554d31c8a05f5fdad57cfe87761.

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/TilesetEntry.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/TilesetEntry.java
@@ -73,16 +73,13 @@ public class TilesetEntry extends CustomPropertyProvider implements ITilesetEntr
     return this.animation;
   }
 
+  @Nullable
   @Override
   public BufferedImage getImage() {
     if (this.animation == null) {
       return this.getBasicImage();
     }
-    ITilesetEntry tileEntry = this.tileset.getTile(this.animation.getCurrentFrame().getTileId());
-    if (tileEntry == null) {
-      return null;
-    }
-    return tileEntry.getBasicImage();
+    return this.tileset.getTile(this.animation.getCurrentFrame().getTileId()).getBasicImage();
   }
 
   @Nullable
