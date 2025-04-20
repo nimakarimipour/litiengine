0b5aecf688777e28d164ffd13884c28adcc0eba7
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-19 23:02:36 -0700

    Revert "fix: 91 - RETURN_NULLABLE - return this.horizontalSlider; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 793e21b7d5e06b0a13eb228ffec5644a2fe28408.

--- src/de/gurkenlabs/litiengine/gui/ListField.java
+++ src/de/gurkenlabs/litiengine/gui/ListField.java
@@ -156,9 +156,6 @@ public class ListField extends GuiComponent {
   }
 
   public HorizontalSlider getHorizontalSlider() {
-    if (this.horizontalSlider == null) {
-      throw new IllegalStateException("HorizontalSlider is not initialized");
-    }
     return this.horizontalSlider;
   }
 
