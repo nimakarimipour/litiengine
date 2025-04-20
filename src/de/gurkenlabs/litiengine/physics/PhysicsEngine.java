536ec68b03e7aaea19bf702a55aa6600554bbeb3
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-19 23:07:34 -0700

    Revert "fix: 151 - PASS_NULLABLE - CollisionEvent event = new CollisionEvent(collider, involvedEntities); - passing @Nullable parameter 'involvedEntities' where @NonNull is required"
    This reverts commit 4b59ffdcfd008c1190ee9227009491115b14af86.

--- src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
+++ src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
@@ -886,13 +886,19 @@ public final class PhysicsEngine implements IUpdateable {
   }
 
   private static void fireCollisionEvents(
-      ICollisionEntity collider, Intersection... intersections) {
+      ICollisionEntity collider, @Nullable Intersection... intersections) {
     // aggregate the involved entities of all intersections
-    ICollisionEntity[] involvedEntities = new ICollisionEntity[0];
+    ICollisionEntity[] involvedEntities = null;
     for (Intersection inter : intersections) {
       if (inter == null) {
         continue;
       }
+
+      if (involvedEntities == null) {
+        involvedEntities = inter.involvedEntities;
+        continue;
+      }
+
       involvedEntities = ArrayUtilities.distinct(involvedEntities, inter.involvedEntities);
     }
 
