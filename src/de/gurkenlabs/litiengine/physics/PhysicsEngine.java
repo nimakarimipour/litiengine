83ea4f265609fbfb70613b94424c3bd5bbb85d30
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-20 17:44:22 -0700

    Revert "fix: 107 - DEREFERENCE_NULLABLE - double leftBoundX = this.getBounds().getMinX() - collisionLocationX; - dereferenced expression this.getBounds() is @Nullable"
    This reverts commit a12fce8d613ad6c9f558070ecdbd9ed250f0d011.

--- src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
+++ src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
@@ -840,10 +840,6 @@ public final class PhysicsEngine implements IUpdateable {
   }
 
   private Point2D clamptoMap(IMobileEntity entity, Point2D newLocation) {
-    if (this.getBounds() == null) {
-      throw new IllegalStateException("Bounds must not be null");
-    }
-
     double collisionLocationX =
         entity.getCollisionBoxAlign().getLocation(entity.getWidth(), entity.getCollisionBoxWidth());
     double leftBoundX = this.getBounds().getMinX() - collisionLocationX;
