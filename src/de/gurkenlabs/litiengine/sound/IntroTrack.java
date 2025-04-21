5f9cca6e2d328ae1dec4ab3d1b6e9e9ab085f54f
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-20 17:47:20 -0700

    fix: 144 - DEREFERENCE_NULLABLE - if (!intro.getFormat().matches(loop.getFormat())) { - dereferenced expression intro.getFormat() is @Nullable

--- src/de/gurkenlabs/litiengine/sound/IntroTrack.java
+++ src/de/gurkenlabs/litiengine/sound/IntroTrack.java
@@ -50,8 +50,10 @@ public class IntroTrack implements Track {
   public IntroTrack(Sound intro, Sound loop) {
     Objects.requireNonNull(intro);
     Objects.requireNonNull(loop);
-    if (!intro.getFormat().matches(loop.getFormat())) {
-      throw new IllegalArgumentException(intro.getFormat() + " does not match " + loop.getFormat());
+    String introFormat = intro.getFormat();
+    String loopFormat = loop.getFormat();
+    if (introFormat == null || loopFormat == null || !introFormat.matches(loopFormat)) {
+      throw new IllegalArgumentException(introFormat + " does not match " + loopFormat);
     }
     this.intro = intro;
     this.loop = loop;
