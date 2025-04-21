41079acbf8125bae99e5f099da738a216fdc188a
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-20 17:37:30 -0700

    Revert "fix: 28 - DEREFERENCE_NULLABLE - this.miscEntities.get(entity.getRenderType()).put(entity.getMapId(), entity); - dereferenced expression this.miscEntities.get(entity.getRenderType()) is @Nullable"
    This reverts commit 1aae2d377922a093f15cf23cb759b980d7d2bfd8.

--- src/de/gurkenlabs/litiengine/environment/Environment.java
+++ src/de/gurkenlabs/litiengine/environment/Environment.java
@@ -325,9 +325,7 @@ public final class Environment implements IRenderable {
       return;
     }
     this.addEntity(entity);
-    if (this.miscEntities.get(entity.getRenderType()) != null) {
-      this.miscEntities.get(entity.getRenderType()).put(entity.getMapId(), entity);
-    }
+    this.miscEntities.get(entity.getRenderType()).put(entity.getMapId(), entity);
     this.fireEntityEvent(l -> l.entityAdded(entity));
   }
 
