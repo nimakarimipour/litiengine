ff6a7ede7309df701141b4d93500b49a52c0b771
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-20 17:41:28 -0700

    Revert "fix: 74 - DEREFERENCE_NULLABLE - final Graphics2D g2 = (Graphics2D) wrapperImage.getGraphics(); - dereferenced expression wrapperImage is @Nullable"
    This reverts commit 4aa34e7384e0ccf0588b9d3857c8706d821fc648.

--- src/de/gurkenlabs/litiengine/util/Imaging.java
+++ src/de/gurkenlabs/litiengine/util/Imaging.java
@@ -505,8 +505,9 @@ public final class Imaging {
     return scale(image, width, height, keepRatio, true);
   }
 
+  @Nullable
   public static BufferedImage scale(
-      final BufferedImage image,
+      @Nullable final BufferedImage image,
       final int width,
       final int height,
       final boolean keepRatio,
@@ -548,9 +549,6 @@ public final class Imaging {
 
     if (fill && (newWidth != width || newHeight != height)) {
       final BufferedImage wrapperImage = getCompatibleImage(width, height);
-      if (wrapperImage == null) {
-        return newImg;
-      }
       final Graphics2D g2 = (Graphics2D) wrapperImage.getGraphics();
       g2.drawImage(
           newImg, (int) ((width - newWidth) / 2.0), (int) ((height - newHeight) / 2.0), null);
