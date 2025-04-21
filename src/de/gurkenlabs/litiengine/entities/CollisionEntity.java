6170204d6cc066a2265a2c37b7df9ae0cbdb5455
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-20 17:39:51 -0700

    Revert "fix: 57 - RETURN_NULLABLE - return this.collisionBox; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit bf40f171511b3ef78516fcd49adc1517468f0663.

--- src/de/gurkenlabs/litiengine/entities/CollisionEntity.java
+++ src/de/gurkenlabs/litiengine/entities/CollisionEntity.java
@@ -89,9 +89,6 @@ public abstract class CollisionEntity extends Entity implements ICollisionEntity
    */
   @Override
   public Rectangle2D getCollisionBox() {
-    if (this.collisionBox == null) {
-      throw new IllegalStateException("Collision box is not initialized");
-    }
     return this.collisionBox;
   }
 
