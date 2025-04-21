df436070a700cd2577e39e4569229583bacb8f71
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-20 17:39:41 -0700

    fix: 55 - DEREFERENCE_NULLABLE - this.getTiles().stream().filter(x -> x.getTileCoordinate().equals(location)).findFirst(); - dereferenced expression x.getTileCoordinate() is @Nullable

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/TileLayer.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/TileLayer.java
@@ -35,12 +35,13 @@ public class TileLayer extends Layer implements ITileLayer {
     this.data = data;
   }
 
-  @Nullable
   @Override
   public ITile getTileByLocation(final Point2D location) {
     final Optional<ITile> tile =
-        this.getTiles().stream().filter(x -> x.getTileCoordinate().equals(location)).findFirst();
-    return tile.isPresent() ? tile.get() : null;
+        this.getTiles().stream()
+            .filter(x -> x.getTileCoordinate() != null && x.getTileCoordinate().equals(location))
+            .findFirst();
+    return tile.orElse(null);
   }
 
   @Nullable
