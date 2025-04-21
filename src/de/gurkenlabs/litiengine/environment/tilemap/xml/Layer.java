2673a5ca0d691e7a48509e473a59d2286d320c89
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-20 17:38:52 -0700

    fix: 44 - RETURN_NULLABLE - return this.parentMap; - returning @Nullable expression from method with @NonNull return type

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/Layer.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/Layer.java
@@ -155,6 +155,9 @@ public abstract class Layer extends CustomPropertyProvider implements ILayer {
 
   @Override
   public IMap getMap() {
+    if (this.parentMap == null) {
+      throw new IllegalStateException("parentMap cannot be null");
+    }
     return this.parentMap;
   }
 
