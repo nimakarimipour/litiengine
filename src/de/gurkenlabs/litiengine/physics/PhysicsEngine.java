d807f29ea5bf458a768fb00190484184c083587b
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-20 17:39:27 -0700

    fix: 52 - DEREFERENCE_NULLABLE - this.collisionEntities.get(entity.getCollisionType()).add(entity); - dereferenced expression this.collisionEntities.get(entity.getCollisionType()) is @Nullable

--- src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
+++ src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
@@ -73,16 +73,15 @@ public final class PhysicsEngine implements IUpdateable {
       return;
     }
 
-    switch (entity.getCollisionType()) {
-      case DYNAMIC:
-      case STATIC:
-        this.collisionEntities.get(entity.getCollisionType()).add(entity);
-        break;
-      default:
-        return;
+    List<ICollisionEntity> entities = this.collisionEntities.get(entity.getCollisionType());
+    if (entities != null) {
+      entities.add(entity);
     }
 
-    this.collisionEntities.get(Collision.ANY).add(entity);
+    List<ICollisionEntity> anyEntities = this.collisionEntities.get(Collision.ANY);
+    if (anyEntities != null) {
+      anyEntities.add(entity);
+    }
   }
 
   /**
