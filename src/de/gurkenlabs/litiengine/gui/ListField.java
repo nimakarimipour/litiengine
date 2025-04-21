441e0b9d54173c53dc1ba8e9dbd40a5a2a16040a
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-20 17:38:49 -0700

    Revert "fix: 43 - DEREFERENCE_NULLABLE - this.getListEntry(column, row) - dereferenced expression this.getListEntry(column, row) is @Nullable"
    This reverts commit 7381f3a27d995b4907f4bf8b1be40fa320745485.

--- src/de/gurkenlabs/litiengine/gui/ListField.java
+++ src/de/gurkenlabs/litiengine/gui/ListField.java
@@ -278,28 +278,21 @@ public class ListField extends GuiComponent {
           if (this.getContent()[column + this.getHorizontalLowerBound()][
                   row + this.getVerticalLowerBound()]
               instanceof Image) {
-            ListEntry entry = this.getListEntry(column, row);
-            if (entry != null) {
-              entry.setImage(
-                  (Image)
-                      this.getContent()[column + this.getHorizontalLowerBound()][
-                          row + this.getVerticalLowerBound()]);
-            }
+            this.getListEntry(column, row)
+                .setImage(
+                    (Image)
+                        this.getContent()[column + this.getHorizontalLowerBound()][
+                            row + this.getVerticalLowerBound()]);
           } else {
-            ListEntry entry = this.getListEntry(column, row);
-            if (entry != null) {
-              entry.setText(
-                  this
-                      .getContent()[column + this.getHorizontalLowerBound()][
-                      row + this.getVerticalLowerBound()]
-                      .toString());
-            }
+            this.getListEntry(column, row)
+                .setText(
+                    this
+                        .getContent()[column + this.getHorizontalLowerBound()][
+                        row + this.getVerticalLowerBound()]
+                        .toString());
           }
         } else {
-          ListEntry entry = this.getListEntry(column, row);
-          if (entry != null) {
-            entry.setText("");
-          }
+          this.getListEntry(column, row).setText("");
         }
       }
     }
