9e936eb292e2a1c8c061af20f79f79c46c4e2be5
Nima Karimipour <karimipour.nima@gmail.com>
2025-04-20 17:36:43 -0700

    fix: 19 - DEREFERENCE_NULLABLE - final Graphics2D g = dimg.createGraphics(); - dereferenced expression dimg is @Nullable

--- src/de/gurkenlabs/litiengine/util/Imaging.java
+++ src/de/gurkenlabs/litiengine/util/Imaging.java
@@ -373,6 +373,9 @@ public final class Imaging {
     }
 
     final BufferedImage dimg = getCompatibleImage(w, h);
+    if (dimg == null) {
+      throw new NullPointerException("The compatible image could not be created.");
+    }
     final Graphics2D g = dimg.createGraphics();
     g.drawImage(img, 0, 0, w, h, w, 0, 0, h, null);
     g.dispose();
