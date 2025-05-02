f9a7c64647d7959fe97ed2e88d5a6370555d260f
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-02 13:34:32 -0700

    fix: 23 - RETURN_NULLABLE - return this.name; - returning @Nullable expression from method with @NonNull return type

--- src/de/gurkenlabs/litiengine/gui/screens/ScreenManager.java
+++ src/de/gurkenlabs/litiengine/gui/screens/ScreenManager.java
@@ -154,6 +154,7 @@ public final class ScreenManager {
     }
 
     if (this.screens.stream()
+        .filter(element -> element.getName() != null)
         .noneMatch(element -> element.getName().equalsIgnoreCase(screenName))) {
       log.log(
           Level.WARNING,
@@ -178,9 +179,14 @@ public final class ScreenManager {
    */
   @Nullable
   public Screen get(String screenName) {
+    if (screenName == null) {
+      return null;
+    }
     Optional<Screen> opt =
         this.screens.stream()
-            .filter(element -> element.getName().equalsIgnoreCase(screenName))
+            .filter(
+                element ->
+                    element.getName() != null && element.getName().equalsIgnoreCase(screenName))
             .findFirst();
     return opt.orElse(null);
   }
