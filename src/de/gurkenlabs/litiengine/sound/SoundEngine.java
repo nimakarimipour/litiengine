c67007ebb52655d2af5d19689739732ee7bafc6b
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 22:12:41 -0700

    Revert "fix: 82 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 80cbcf408faef2c611e20cf82930a53a97cb34ff.

--- src/de/gurkenlabs/litiengine/sound/SoundEngine.java
+++ src/de/gurkenlabs/litiengine/sound/SoundEngine.java
@@ -646,14 +646,14 @@ public final class SoundEngine implements IUpdateable, ILaunchable {
   }
 
   private SFXPlayback playSound(
-      Sound sound, Supplier<Point2D> supplier, boolean loop, int range, float volume) {
+      @Nullable Sound sound, Supplier<Point2D> supplier, boolean loop, int range, float volume) {
     if (sound == null) {
-      return new SFXPlayback(); // Assuming a default non-null SFXPlayback instance creation
+      return null;
     }
 
     SFXPlayback playback = createSound(sound, supplier, loop, range, volume);
     if (playback == null) {
-      return new SFXPlayback(); // Assuming a default non-null SFXPlayback instance creation
+      return null;
     }
     playback.start();
     return playback;
