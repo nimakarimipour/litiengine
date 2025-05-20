5f078280cd36108e1d09ce8da5d0ac958dde32c8
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 22:07:45 -0700

    Revert "fix: 25 - DEREFERENCE_NULLABLE - this.getStaticShadowLayer().render(g); - dereferenced expression this.getStaticShadowLayer() is @Nullable"
    This reverts commit 8356c0bab2514ca6fcf9d5ecdc6960dbb86986a1.

--- src/de/gurkenlabs/litiengine/environment/Environment.java
+++ src/de/gurkenlabs/litiengine/environment/Environment.java
@@ -2146,9 +2146,8 @@ public final class Environment implements IRenderable {
       final double ambientTime = TimeUtilities.nanoToMs(System.nanoTime() - ambientStart);
 
       long shadowRenderStart = System.nanoTime();
-      if (this.getStaticShadowLayer() != null
-          && this.getStaticShadows().stream()
-              .anyMatch(x -> x.getShadowType() != StaticShadowType.NONE)) {
+      if (this.getStaticShadows().stream()
+          .anyMatch(x -> x.getShadowType() != StaticShadowType.NONE)) {
         this.getStaticShadowLayer().render(g);
       }
 
