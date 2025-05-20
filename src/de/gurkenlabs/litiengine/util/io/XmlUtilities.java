7168436673ab94912eaaf5d7b12bcd4ffe028b39
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 22:15:02 -0700

    Revert "fix: 111 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 54b0fcf7a8bff4954644be3e97211ca8d895639d.

--- src/de/gurkenlabs/litiengine/util/io/XmlUtilities.java
+++ src/de/gurkenlabs/litiengine/util/io/XmlUtilities.java
@@ -80,8 +80,9 @@ public final class XmlUtilities {
       return jaxbContext;
     } catch (final JAXBException e) {
       log.log(Level.SEVERE, e.getMessage(), e);
-      throw new RuntimeException("Failed to create JAXBContext for class: " + cls.getName(), e);
     }
+
+    return null;
   }
 
   @Nullable
