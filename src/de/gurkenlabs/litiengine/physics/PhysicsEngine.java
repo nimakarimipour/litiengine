79be8f80d7187a469fe4cd3eb514da2c4bcf3c4f
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 22:18:31 -0700

    Revert "fix: 151 - DEREFERENCE_NULLABLE - for (ICollisionEntity involved : involvedEntities) { - enhanced-for expression involvedEntities is @Nullable"
    This reverts commit 5bfee84df895a76ff21158593a2719d544343bad.

--- src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
+++ src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
@@ -886,20 +886,27 @@ public final class PhysicsEngine implements IUpdateable {
   }
 
   private static void fireCollisionEvents(
-      ICollisionEntity collider, Intersection... intersections) {
-    ICollisionEntity[] involvedEntities = new ICollisionEntity[0];
+      ICollisionEntity collider, @Nullable Intersection... intersections) {
+    // aggregate the involved entities of all intersections
+    ICollisionEntity[] involvedEntities = null;
     for (Intersection inter : intersections) {
       if (inter == null) {
         continue;
       }
-      if (involvedEntities.length == 0) {
+
+      if (involvedEntities == null) {
         involvedEntities = inter.involvedEntities;
         continue;
       }
+
       involvedEntities = ArrayUtilities.distinct(involvedEntities, inter.involvedEntities);
     }
+
+    // 1. fire collision event on the collider with all the involved entities
     CollisionEvent event = new CollisionEvent(collider, involvedEntities);
     collider.fireCollisionEvent(event);
+
+    // 2. fire collision event on the involved entities with the collider entity
     CollisionEvent colliderEvent = new CollisionEvent(collider);
     for (ICollisionEntity involved : involvedEntities) {
       involved.fireCollisionEvent(colliderEvent);
