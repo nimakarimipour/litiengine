70ad551db2ef4a3eaa095ad2ad861a7f98cb7257
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 22:18:11 -0700

    Revert "fix: 147 - DEREFERENCE_NULLABLE - if (loadedEmitters.containsKey(emitterData.getName())) { - dereferenced expression emitterData is @Nullable"
    This reverts commit 2cbf52dbbd1970c198933ccbcf0894fbc5599344.

--- src/de/gurkenlabs/litiengine/graphics/emitters/xml/EmitterLoader.java
+++ src/de/gurkenlabs/litiengine/graphics/emitters/xml/EmitterLoader.java
@@ -47,13 +47,11 @@ public class EmitterLoader {
     return load(loaded);
   }
 
-  public static EmitterData load(EmitterData emitterData) {
-    if (emitterData == null || emitterData.getName() == null) {
-      return null;
-    }
+  public static EmitterData load(@Nullable EmitterData emitterData) {
     if (loadedEmitters.containsKey(emitterData.getName())) {
       return loadedEmitters.get(emitterData.getName());
     }
+
     loadedEmitters.put(emitterData.getName(), emitterData);
     return emitterData;
   }
