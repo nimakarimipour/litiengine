55abfe6ea6429c2361c51932fa305f2bb2fe37c0
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 22:13:48 -0700

    Revert "fix: 96 - DEREFERENCE_NULLABLE - if (prefix != null && spriteSheet.equalsIgnoreCase(prefix)) { - dereferenced expression spriteSheet is @Nullable"
    This reverts commit 422c1054e727b8a894c2405fc989bc1cd7f28bc1.

--- src/de/gurkenlabs/litiengine/environment/CreatureMapObjectLoader.java
+++ src/de/gurkenlabs/litiengine/environment/CreatureMapObjectLoader.java
@@ -77,12 +77,10 @@ public class CreatureMapObjectLoader extends MapObjectLoader {
     return entities;
   }
 
-  protected Creature createNewCreature(IMapObject mapObject, String spriteSheet) {
-    if (spriteSheet == null) {
-      // Handle the case where spriteSheet is null appropriately.
-      return new Creature("defaultSprite"); // or any suitable default value or behavior.
-    }
-
+  protected Creature createNewCreature(IMapObject mapObject, @Nullable String spriteSheet) {
+    // for each known custom creature type, check if it was registered for the specified
+    // spriteSheetName
+    // if so: create an instance of the custom class instead of the default Creature class
     for (Class<? extends Creature> customCreature : customCreatureType) {
       for (String prefix : EntityAnimationController.getDefaultSpritePrefixes(customCreature)) {
         if (prefix != null && spriteSheet.equalsIgnoreCase(prefix)) {
@@ -94,6 +92,8 @@ public class CreatureMapObjectLoader extends MapObjectLoader {
       }
     }
 
+    // if no custom creature type war registered for the spriteSheet, we just create a new Creature
+    // instance
     return new Creature(spriteSheet);
   }
 
