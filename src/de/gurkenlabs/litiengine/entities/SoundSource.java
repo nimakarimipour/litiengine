b9aad96fe3f4fdae4f44f3d0ec17c39dac55aafb
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 22:13:29 -0700

    Revert "fix: 93 - METHOD_NO_INIT - public SoundSource(double x, double y) { - initializer method does not guarantee @NonNull fields sound (line 29), playback (line 30) are initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 2cc2a2e78a6ea47961a3e30f0670c86a399e5b06.

--- src/de/gurkenlabs/litiengine/entities/SoundSource.java
+++ src/de/gurkenlabs/litiengine/entities/SoundSource.java
@@ -63,7 +63,6 @@ public class SoundSource extends Entity {
   public SoundSource(double x, double y) {
     this.setX(x);
     this.setY(y);
-    this.setSound(Resources.sounds().getDefaultSound());
   }
 
   /**
