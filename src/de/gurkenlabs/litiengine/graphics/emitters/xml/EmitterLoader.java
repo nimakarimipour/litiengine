f9e2f0d5b77ed569229084c5417c2c52d2988ede
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 22:27:40 -0700

    Revert "fix: 58 - DEREFERENCE_NULLABLE - final String name = emitterXml.getFile(); - dereferenced expression emitterXml is @Nullable"
    This reverts commit 8604867b600166461201f70f5268b22e535656b8.

--- src/de/gurkenlabs/litiengine/graphics/emitters/xml/EmitterLoader.java
+++ src/de/gurkenlabs/litiengine/graphics/emitters/xml/EmitterLoader.java
@@ -29,8 +29,9 @@ public class EmitterLoader {
     return load(Resources.getLocation(emitterXml));
   }
 
-  public static EmitterData load(URL emitterXml) {
-    final String name = NullabilityUtil.castToNonnull(emitterXml, "Null checks in place").getFile();
+  @Nullable
+  public static EmitterData load(@Nullable URL emitterXml) {
+    final String name = emitterXml.getFile();
     if (loadedEmitters.containsKey(name)) {
       return loadedEmitters.get(name);
     }
