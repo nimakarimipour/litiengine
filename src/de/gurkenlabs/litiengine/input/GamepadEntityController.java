dd807c688a81e1f40156c112554ee6d0f021bb78
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 22:20:39 -0700

    Revert "fix: 5 - RETURN_NULLABLE - return gamePadManager; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 10f451c5f51606c0cab0b0424c16941f78312c0e.

--- src/de/gurkenlabs/litiengine/input/GamepadEntityController.java
+++ src/de/gurkenlabs/litiengine/input/GamepadEntityController.java
@@ -14,35 +14,30 @@ public class GamepadEntityController<T extends IMobileEntity> extends MovementCo
 
   public GamepadEntityController(final T entity, boolean rotateWithRightStick) {
     super(entity);
-    GamepadManager gamepadManager = Input.gamepads();
-
-    if (gamepadManager != null && gamepadManager.current() != null) {
-      this.gamepadId = gamepadManager.current().getId();
-    } else {
-      this.gamepadId = -1; // or any other default value you'd like
+    if (Input.gamepads().current() != null) {
+      this.gamepadId = Input.gamepads().current().getId();
     }
 
     this.rotateWithRightStick = rotateWithRightStick;
-
-    if (gamepadManager != null) {
-      gamepadManager.onAdded(
-          pad -> {
-            if (this.gamepadId == -1) {
-              this.gamepadId = pad.getId();
-            }
-          });
-
-      gamepadManager.onRemoved(
-          pad -> {
-            if (this.gamepadId == pad.getId()) {
-              this.gamepadId = -1;
-              Gamepad newGamePad = gamepadManager.current();
-              if (newGamePad != null) {
-                this.gamepadId = newGamePad.getId();
+    Input.gamepads()
+        .onAdded(
+            pad -> {
+              if (this.gamepadId == -1) {
+                this.gamepadId = pad.getId();
               }
-            }
-          });
-    }
+            });
+
+    Input.gamepads()
+        .onRemoved(
+            pad -> {
+              if (this.gamepadId == pad.getId()) {
+                this.gamepadId = -1;
+                final Gamepad newGamePad = Input.gamepads().current();
+                if (newGamePad != null) {
+                  this.gamepadId = newGamePad.getId();
+                }
+              }
+            });
   }
 
   @Override
@@ -82,13 +77,8 @@ public class GamepadEntityController<T extends IMobileEntity> extends MovementCo
       return;
     }
 
-    GamepadManager gamepadManager = Input.gamepads();
-    if (gamepadManager == null) {
-      return;
-    }
-
-    final float x = gamepadManager.get(this.gamepadId).getPollData(Gamepad.Axis.X);
-    final float y = gamepadManager.get(this.gamepadId).getPollData(Gamepad.Axis.Y);
+    final float x = Input.gamepads().get(this.gamepadId).getPollData(Gamepad.Axis.X);
+    final float y = Input.gamepads().get(this.gamepadId).getPollData(Gamepad.Axis.Y);
 
     if (Math.abs(x) > this.gamepadDeadzone) {
       this.setDx(x);
@@ -99,8 +89,8 @@ public class GamepadEntityController<T extends IMobileEntity> extends MovementCo
     }
 
     if (this.isRotateWithRightStick()) {
-      final float rightX = gamepadManager.get(this.gamepadId).getPollData(Gamepad.Axis.RX);
-      final float rightY = gamepadManager.get(this.gamepadId).getPollData(Gamepad.Axis.RY);
+      final float rightX = Input.gamepads().get(this.gamepadId).getPollData(Gamepad.Axis.RX);
+      final float rightY = Input.gamepads().get(this.gamepadId).getPollData(Gamepad.Axis.RY);
       float targetX = 0;
       float targetY = 0;
       if (Math.abs(rightX) > this.gamepadRightStick) {
