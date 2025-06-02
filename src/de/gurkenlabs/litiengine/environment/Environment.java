95e2a11acda10d71d4ad31ab850688474101b943
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 22:25:33 -0700

    fix: 33 - DEREFERENCE_NULLABLE - entity.movement().apply(force); - dereferenced expression entity.movement() is @Nullable

--- src/de/gurkenlabs/litiengine/environment/Environment.java
+++ src/de/gurkenlabs/litiengine/environment/Environment.java
@@ -2383,7 +2383,7 @@ public final class Environment implements IRenderable {
     if (mvmtControl != null) {
       GravityForce force = new GravityForce(entity, this.getGravity(), Direction.DOWN);
       force.setIdentifier(GRAVITY_IDENTIFIER);
-      entity.movement().apply(force);
+      NullabilityUtil.castToNonnull(entity.movement(), "null-check for mvmtControl").apply(force);
       this.gravityForces.put(entity.getMapId(), force);
     }
   }
