99c03e096b7575f022cb17f0ba500d193758561a
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-11 21:42:26 -0700

    fix: 7 - DEREFERENCE_NULLABLE - return this.tileset - dereferenced expression this.tileset         .getSpritesheet() is @Nullable

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/TilesetEntry.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/TilesetEntry.java
@@ -88,9 +88,12 @@ public class TilesetEntry extends CustomPropertyProvider implements ITilesetEntr
     if (this.image != null) {
       return Resources.images().get(this.image.getAbsoluteSourcePath());
     }
-    return this.tileset
-        .getSpritesheet()
-        .getSprite(this.getId(), this.tileset.getMargin(), this.tileset.getSpacing());
+    Spritesheet spritesheet = this.tileset != null ? this.tileset.getSpritesheet() : null;
+    if (spritesheet != null) {
+      return spritesheet.getSprite(
+          this.getId(), this.tileset.getMargin(), this.tileset.getSpacing());
+    }
+    return null;
   }
 
   @Override
