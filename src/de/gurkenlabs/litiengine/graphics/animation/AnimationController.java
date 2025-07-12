8ab1cc6394132b523b0a5b94cd6acdc6df8c16dc
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-11 22:01:51 -0700

    fix: 60 - PASS_NULLABLE - sprite = effect.apply(sprite); - passing @Nullable parameter 'sprite' where @NonNull is required

--- src/de/gurkenlabs/litiengine/graphics/animation/AnimationController.java
+++ src/de/gurkenlabs/litiengine/graphics/animation/AnimationController.java
@@ -6,6 +6,7 @@ import de.gurkenlabs.litiengine.graphics.ImageEffect;
 import de.gurkenlabs.litiengine.graphics.Spritesheet;
 import de.gurkenlabs.litiengine.resources.Resources;
 import de.gurkenlabs.litiengine.util.Imaging;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
@@ -207,7 +208,7 @@ public class AnimationController implements IAnimationController {
     BufferedImage sprite =
         current.getSpritesheet().getSprite(current.getCurrentKeyFrame().getSpriteIndex());
     for (final ImageEffect effect : this.getImageEffects()) {
-      sprite = effect.apply(sprite);
+      sprite = effect.apply(Nullability.castToNonnull(sprite));
     }
 
     return sprite;
