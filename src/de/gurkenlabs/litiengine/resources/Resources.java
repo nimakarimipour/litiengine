be0d31eb4d86d009868f21781828d127baec4d37
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-11 22:15:09 -0700

    fix: 121 - DEREFERENCE_NULLABLE - return file.openStream(); - dereferenced expression file is @Nullable

--- src/de/gurkenlabs/litiengine/resources/Resources.java
+++ src/de/gurkenlabs/litiengine/resources/Resources.java
@@ -363,6 +363,9 @@ public final class Resources {
 
   @Nullable
   private static InputStream getResource(@Nullable final URL file) {
+    if (file == null) {
+      return null;
+    }
     try {
       return file.openStream();
     } catch (IOException e) {
