2a21d7c10f04fdc1a81f3b8b0a10a607cdd68dae
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-11 21:44:47 -0700

    Revert "fix: 10 - DEREFERENCE_NULLABLE - final Graphics2D g = this.layer.createGraphics(); - dereferenced expression this.layer is @Nullable"
    This reverts commit 5005e95aeb38608f747b32eb26306fcbf91cbd0b.

--- src/de/gurkenlabs/litiengine/graphics/ColorLayer.java
+++ src/de/gurkenlabs/litiengine/graphics/ColorLayer.java
@@ -53,7 +53,7 @@ public abstract class ColorLayer implements IRenderable {
   }
 
   public void updateSection(Rectangle2D section) {
-    if (this.getColor() == null || this.layer == null) {
+    if (this.getColor() == null) {
       return;
     }
 
