e288de61689f1b8674cf43d8d17168243cc941f8
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-11 21:52:19 -0700

    fix: 25 - DEREFERENCE_NULLABLE - this.getStaticShadowLayer().render(g); - dereferenced expression this.getStaticShadowLayer() is @Nullable

--- src/de/gurkenlabs/litiengine/environment/Environment.java
+++ src/de/gurkenlabs/litiengine/environment/Environment.java
@@ -2146,8 +2146,8 @@ public final class Environment implements IRenderable {
       final double ambientTime = TimeUtilities.nanoToMs(System.nanoTime() - ambientStart);
 
       long shadowRenderStart = System.nanoTime();
-      if (this.getStaticShadows().stream()
-          .anyMatch(x -> x.getShadowType() != StaticShadowType.NONE)) {
+      if (this.getStaticShadows().stream().anyMatch(x -> x.getShadowType() != StaticShadowType.NONE)
+          && this.getStaticShadowLayer() != null) {
         this.getStaticShadowLayer().render(g);
       }
 
