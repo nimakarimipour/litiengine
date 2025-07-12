bc808e1eb14a90ec769481e3976c540e28a4fb60
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-11 21:57:57 -0700

    fix: 52 - DEREFERENCE_NULLABLE - this.collisionEntities.get(entity.getCollisionType()).add(entity); - dereferenced expression this.collisionEntities.get(entity.getCollisionType()) is @Nullable

--- src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
+++ src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
@@ -8,6 +8,7 @@ import de.gurkenlabs.litiengine.entities.IMobileEntity;
 import de.gurkenlabs.litiengine.util.ArrayUtilities;
 import de.gurkenlabs.litiengine.util.MathUtilities;
 import de.gurkenlabs.litiengine.util.geom.GeometricUtilities;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.awt.Shape;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
@@ -76,7 +77,9 @@ public final class PhysicsEngine implements IUpdateable {
     switch (entity.getCollisionType()) {
       case DYNAMIC:
       case STATIC:
-        this.collisionEntities.get(entity.getCollisionType()).add(entity);
+        Nullability.castToNonnull(
+                this.collisionEntities.get(entity.getCollisionType()), "collision type mapped")
+            .add(entity);
         break;
       default:
         return;
