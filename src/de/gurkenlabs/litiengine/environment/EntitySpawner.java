acf7e74b466c1cc5ad34d73385b8197120f4c50a
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-11 22:17:16 -0700

    Revert "fix: 134 - RETURN_NULLABLE - return this.spawnpoints; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 2738489b02d27c26729320f859ec8e733d3ef264.

--- src/de/gurkenlabs/litiengine/environment/EntitySpawner.java
+++ src/de/gurkenlabs/litiengine/environment/EntitySpawner.java
@@ -3,7 +3,6 @@ package de.gurkenlabs.litiengine.environment;
 import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.entities.IEntity;
 import de.gurkenlabs.litiengine.entities.Spawnpoint;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -101,7 +100,6 @@ public abstract class EntitySpawner<T extends IEntity> implements IEntitySpawner
     return this.spawnMode;
   }
 
-  @Nullable
   @Override
   public List<Spawnpoint> getSpawnPoints() {
     return this.spawnpoints;
@@ -153,48 +151,39 @@ public abstract class EntitySpawner<T extends IEntity> implements IEntitySpawner
    * @see SpawnMode
    */
   protected void spawnNewEntities() {
-    List<Spawnpoint> spawnPoints = this.getSpawnPoints();
-    if (this.getSpawnMode() != SpawnMode.CUSTOMSPAWNPOINTS
-        && (spawnPoints == null || spawnPoints.isEmpty())) {
+    if (this.getSpawnMode() != SpawnMode.CUSTOMSPAWNPOINTS && this.getSpawnPoints().isEmpty()) {
       return;
     }
 
     switch (this.getSpawnMode()) {
       case ALLSPAWNPOINTS:
-        for (int i = 0;
-            i
-                < Nullability.castToNonnull(this.getSpawnPoints(), "spawnPoints checked initially")
-                    .size();
-            i++) {
+        for (int i = 0; i < this.getSpawnPoints().size(); i++) {
           final int index = i;
           Game.loop()
               .perform(
                   this.getSpawnDelay() + this.getSpawnDelay() * i,
-                  () ->
-                      this.spawn(
-                          Nullability.castToNonnull(spawnPoints, "checked before if").get(index),
-                          this.getSpawnAmount()));
+                  () -> this.spawn(this.getSpawnPoints().get(index), this.getSpawnAmount()));
         }
         break;
       case ONERANDOMSPAWNPOINT:
-        this.spawn(Game.random().choose(spawnPoints), this.getSpawnAmount());
+        this.spawn(Game.random().choose(this.getSpawnPoints()), this.getSpawnAmount());
         break;
       case RANDOMSPAWNPOINTS:
         for (int i = 0; i < this.getSpawnAmount(); i++) {
           Game.loop()
               .perform(
                   this.getSpawnDelay() + this.getSpawnDelay() * i,
-                  () -> this.spawn(Game.random().choose(spawnPoints), 1));
+                  () -> this.spawn(Game.random().choose(this.getSpawnPoints()), 1));
         }
         break;
       case CUSTOMSPAWNPOINTS:
-        List<Spawnpoint> customSpawnPoints =
+        List<Spawnpoint> spawnPoints =
             this.customSpawnpoints != null
                 ? this.customSpawnpoints.apply(this)
                 : this.getCustomSpawnpoints();
 
         int index = 0;
-        for (Spawnpoint spawn : customSpawnPoints) {
+        for (Spawnpoint spawn : spawnPoints) {
           Game.loop()
               .perform(
                   this.getSpawnDelay() + this.getSpawnDelay() * index, () -> this.spawn(spawn, 1));
