025376352c42d46250f5be45404caaf646442a4f
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 11:41:17 -0700

    fix: 44 - RETURN_NULLABLE - return this.camera; - returning @Nullable expression from method with @NonNull return type

--- src/de/gurkenlabs/litiengine/environment/GameWorld.java
+++ src/de/gurkenlabs/litiengine/environment/GameWorld.java
@@ -228,6 +228,9 @@ public final class GameWorld implements IUpdateable {
    * @see ICamera
    */
   public ICamera camera() {
+    if (this.camera == null) {
+      throw new NullPointerException("Camera cannot be null");
+    }
     return this.camera;
   }
 
