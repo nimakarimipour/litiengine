90f6998221ab1f767537fb4f24f0769fb10003df
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:11:23 -0700

    fix: 152 - RETURN_NULLABLE - return this.color; - returning @Nullable expression from method with @NonNull return type

--- src/de/gurkenlabs/litiengine/graphics/ColorLayer.java
+++ src/de/gurkenlabs/litiengine/graphics/ColorLayer.java
@@ -4,6 +4,7 @@ import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.environment.Environment;
 import de.gurkenlabs.litiengine.util.Imaging;
 import de.gurkenlabs.litiengine.util.MathUtilities;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
@@ -33,6 +34,7 @@ public abstract class ColorLayer implements IRenderable {
     ImageRenderer.render(g, this.layer, -viewport.getX(), -viewport.getY());
   }
 
+  @Nullable
   public Color getColor() {
     return this.color;
   }
@@ -40,7 +42,7 @@ public abstract class ColorLayer implements IRenderable {
   public void setAlpha(int ambientAlpha) {
     this.setColor(
         new Color(
-            this.getColor().getRed(),
+            Nullability.castToNonnull(this.getColor(), "initialized before use").getRed(),
             this.getColor().getGreen(),
             this.getColor().getBlue(),
             MathUtilities.clamp(ambientAlpha, 0, 255)));
