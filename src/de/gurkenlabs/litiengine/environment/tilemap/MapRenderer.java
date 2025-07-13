a3c2cbeb72b032e7fbd92a8e15884a061ae9476c
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:02:32 -0700

    fix: 76 - METHOD_NO_INIT - public TilesetEntry() {} - initializer method does not guarantee @NonNull field tileset (line 23) is initialized along all control-flow paths (remember to check for exceptions or early returns).

--- src/de/gurkenlabs/litiengine/environment/tilemap/MapRenderer.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/MapRenderer.java
@@ -164,10 +164,16 @@ public class MapRenderer {
     if (image != null) {
       Point p = map.getOrientation().getLocation(x, y, map);
       p.y -= image.getHeight();
-      ITileOffset offset = tile.getTilesetEntry().getTileset().getTileOffset();
-      if (offset != null) {
-        p.x += offset.getX();
-        p.y += offset.getY();
+      ITilesetEntry tilesetEntry = tile.getTilesetEntry();
+      if (tilesetEntry != null) {
+        ITileset tileset = tilesetEntry.getTileset();
+        if (tileset != null) {
+          ITileOffset offset = tileset.getTileOffset();
+          if (offset != null) {
+            p.x += offset.getX();
+            p.y += offset.getY();
+          }
+        }
       }
       if (viewport.intersects(p.x, p.y, image.getWidth(), image.getHeight())) {
         ImageRenderer.render(g, image, p.x - viewport.getX(), p.y - viewport.getY());
