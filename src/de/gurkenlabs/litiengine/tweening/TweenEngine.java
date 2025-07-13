5a1311db9f8f231a1de2f702d25e1320098585a9
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 11:46:04 -0700

    fix: 106 - DEREFERENCE_NULLABLE - this.getTweens().get(target).remove(type); - dereferenced expression this.getTweens().get(target) is @Nullable

--- src/de/gurkenlabs/litiengine/tweening/TweenEngine.java
+++ src/de/gurkenlabs/litiengine/tweening/TweenEngine.java
@@ -121,7 +121,10 @@ public class TweenEngine implements IUpdateable, ILaunchable {
   public void remove(final Tweenable target, final TweenType type) {
     final Tween tween = this.getTween(target, type);
     if (tween != null) {
-      this.getTweens().get(target).remove(type);
+      final Map<TweenType, ?> tweensForTarget = this.getTweens().get(target);
+      if (tweensForTarget != null) {
+        tweensForTarget.remove(type);
+      }
     }
   }
 
