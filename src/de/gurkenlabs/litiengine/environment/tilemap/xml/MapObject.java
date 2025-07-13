687d07ed9fe557cdc2f72a04da9c06cddbb100d6
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 11:42:46 -0700

    Revert "fix: 63 - UNBOX_NULLABLE - return this.x - map.getChunkOffsetX() * map.getTileWidth(); - unboxing of a @Nullable value - this.x"
    This reverts commit d1f933924fcf5efa43039459f3a14ebf2b6f127f.

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/MapObject.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/MapObject.java
@@ -326,7 +326,7 @@ public class MapObject extends CustomPropertyProvider implements IMapObject {
   public float getX() {
     if (this.isInfiniteMap()) {
       TmxMap map = (TmxMap) this.getLayer().getMap();
-      return (this.x == null ? 0 : this.x) - map.getChunkOffsetX() * map.getTileWidth();
+      return this.x - map.getChunkOffsetX() * map.getTileWidth();
     }
 
     return this.x == null ? 0 : this.x;
