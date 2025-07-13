a94398ce41d06c7abbfb0a3f3f4ceced9c8518f8
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 11:47:07 -0700

    Revert "fix: 119 - DEREFERENCE_NULLABLE - for (final ICollisionEntity collisionEntity : this.collisionEntities.get(collision)) { - enhanced-for expression this.collisionEntities.get(collision) is @Nullable"
    This reverts commit 22fdc2c2cf0a0bd44c73245d109338c10c7d03a4.

--- src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
+++ src/de/gurkenlabs/litiengine/physics/PhysicsEngine.java
@@ -541,13 +541,8 @@ public final class PhysicsEngine implements IUpdateable {
   @Nullable
   public RaycastHit raycast(Line2D line, Collision collision, @Nullable ICollisionEntity entity) {
     final Point2D rayCastSource = new Point2D.Double(line.getX1(), line.getY1());
-    Collection<ICollisionEntity> collisionEntitiesForType = this.collisionEntities.get(collision);
 
-    if (collisionEntitiesForType == null) {
-      return null;
-    }
-
-    for (final ICollisionEntity collisionEntity : collisionEntitiesForType) {
+    for (final ICollisionEntity collisionEntity : this.collisionEntities.get(collision)) {
       if (!canCollide(entity, collisionEntity)) {
         continue;
       }
