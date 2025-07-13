d28575f1c68ba5ddc591eecdcfd0453fdaecbf57
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 11:47:03 -0700

    Revert "fix: 118 - DEREFERENCE_NULLABLE - String enc = value.trim(); - dereferenced expression value is @Nullable"
    This reverts commit f79bf5afe3a2864cfc4c54fadd9c8016f434bd65.

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/TileData.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/TileData.java
@@ -285,10 +285,6 @@ public class TileData {
 
   protected static List<Tile> parseBase64Data(@Nullable String value, @Nullable String compression)
       throws InvalidTileLayerException {
-    if (value == null) {
-      throw new InvalidTileLayerException("Input value is null");
-    }
-
     List<Tile> parsed = new ArrayList<>();
 
     String enc = value.trim();
