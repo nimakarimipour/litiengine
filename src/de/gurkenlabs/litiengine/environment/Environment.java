5a3322e52ad570f71a07decdb4c681a244fd2564
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 11:54:22 -0700

    Revert "fix: 22 - DEREFERENCE_NULLABLE - this.renderables.get(renderType).add(renderable); - dereferenced expression this.renderables.get(renderType) is @Nullable"
    This reverts commit 9e52fbd4fec1e83008e7807d4607f489076ba3b9.

--- src/de/gurkenlabs/litiengine/environment/Environment.java
+++ src/de/gurkenlabs/litiengine/environment/Environment.java
@@ -43,7 +43,6 @@ import de.gurkenlabs.litiengine.physics.IMovementController;
 import de.gurkenlabs.litiengine.resources.Resources;
 import de.gurkenlabs.litiengine.util.TimeUtilities;
 import de.gurkenlabs.litiengine.util.geom.GeometricUtilities;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Shape;
@@ -411,7 +410,7 @@ public final class Environment implements IRenderable {
    * @see RenderEngine#renderEntity(Graphics2D, IEntity)
    */
   public void add(IRenderable renderable, RenderType renderType) {
-    Nullability.castToNonnull(this.renderables.get(renderType)).add(renderable);
+    this.renderables.get(renderType).add(renderable);
   }
 
   /**
