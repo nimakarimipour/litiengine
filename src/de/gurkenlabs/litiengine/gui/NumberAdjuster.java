2149475907f845feb60ca0ed29f6096302b0cfd4
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 11:50:11 -0700

    fix: 1 - RETURN_NULLABLE - return this.currentValue; - returning @Nullable expression from method with @NonNull return type

--- src/de/gurkenlabs/litiengine/gui/NumberAdjuster.java
+++ src/de/gurkenlabs/litiengine/gui/NumberAdjuster.java
@@ -1,5 +1,6 @@
 package de.gurkenlabs.litiengine.gui;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -37,9 +38,12 @@ public class NumberAdjuster extends TextFieldComponent {
   }
 
   public void decrement() {
-    this.setCurrentValue(this.getCurrentValue().subtract(this.getStepSize()));
+    this.setCurrentValue(
+        Nullability.castToNonnull(this.getCurrentValue(), "never set to null")
+            .subtract(this.getStepSize()));
   }
 
+  @Nullable
   public BigDecimal getCurrentValue() {
     return this.currentValue;
   }
@@ -57,7 +61,9 @@ public class NumberAdjuster extends TextFieldComponent {
   }
 
   public void increment() {
-    this.setCurrentValue(this.getCurrentValue().add(this.getStepSize()));
+    this.setCurrentValue(
+        Nullability.castToNonnull(this.getCurrentValue(), "never explicitly set null")
+            .add(this.getStepSize()));
   }
 
   public void onValueChange(final Consumer<BigDecimal> cons) {
@@ -112,7 +118,9 @@ public class NumberAdjuster extends TextFieldComponent {
 
   public void setLowerBound(final BigDecimal lowerBound) {
     this.lowerBound = lowerBound;
-    if (this.getCurrentValue().compareTo(this.getLowerBound()) < 0) {
+    if (Nullability.castToNonnull(this.getCurrentValue(), "ensured non-null state")
+            .compareTo(this.getLowerBound())
+        < 0) {
       this.setCurrentValue(this.getLowerBound());
     }
   }
@@ -123,7 +131,9 @@ public class NumberAdjuster extends TextFieldComponent {
 
   public void setUpperBound(final BigDecimal upperBound) {
     this.upperBound = upperBound;
-    if (this.getCurrentValue().compareTo(this.getUpperBound()) > 0) {
+    if (Nullability.castToNonnull(this.getCurrentValue(), "always not null")
+            .compareTo(this.getUpperBound())
+        > 0) {
       this.setCurrentValue(this.getUpperBound());
     }
   }
