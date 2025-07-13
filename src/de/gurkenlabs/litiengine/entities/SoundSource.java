d590eb2f0aca244126afce23c7173d33b6fcc207
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 11:37:54 -0700

    Revert "fix: 2 - METHOD_NO_INIT - public SoundSource() { - initializer method does not guarantee @NonNull fields sound (line 29), playback (line 30) are initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit c29873253086977f9f918d879e460d71bd2408f2.

--- src/de/gurkenlabs/litiengine/entities/SoundSource.java
+++ src/de/gurkenlabs/litiengine/entities/SoundSource.java
@@ -32,8 +32,6 @@ public class SoundSource extends Entity {
   /** An empty constructor that just calls the super constructor of {@link Entity}. */
   public SoundSource() {
     super();
-    this.sound = new Sound(); // Assuming Sound has a default constructor
-    this.playback = new SFXPlayback(); // Assuming SFXPlayback has a default constructor
   }
 
   /**
