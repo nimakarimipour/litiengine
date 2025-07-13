d3e9d26ac54b01a7ad0b488ba9fea8d200da2a83
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 11:55:00 -0700

    fix: 28 - DEREFERENCE_NULLABLE - this.miscEntities.get(entity.getRenderType()).put(entity.getMapId(), entity); - dereferenced expression this.miscEntities.get(entity.getRenderType()) is @Nullable

--- src/de/gurkenlabs/litiengine/environment/Environment.java
+++ src/de/gurkenlabs/litiengine/environment/Environment.java
@@ -43,6 +43,7 @@ import de.gurkenlabs.litiengine.physics.IMovementController;
 import de.gurkenlabs.litiengine.resources.Resources;
 import de.gurkenlabs.litiengine.util.TimeUtilities;
 import de.gurkenlabs.litiengine.util.geom.GeometricUtilities;
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Shape;
@@ -325,7 +326,8 @@ public final class Environment implements IRenderable {
       return;
     }
     this.addEntity(entity);
-    this.miscEntities.get(entity.getRenderType()).put(entity.getMapId(), entity);
+    Nullability.castToNonnull(this.miscEntities.get(entity.getRenderType()))
+        .put(entity.getMapId(), entity);
     this.fireEntityEvent(l -> l.entityAdded(entity));
   }
 
