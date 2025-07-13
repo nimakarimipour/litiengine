5776757dd84afcb295bc2536caf6b481543dabde
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:08:51 -0700

    Revert "fix: 128 - UNBOX_NULLABLE - return this.y - map.getChunkOffsetY() * map.getTileHeight(); - unboxing of a @Nullable value - this.y"
    This reverts commit b55eed1d24d401ac054bc356c081a198d4b50f0f.

--- src/de/gurkenlabs/litiengine/environment/tilemap/xml/MapObject.java
+++ src/de/gurkenlabs/litiengine/environment/tilemap/xml/MapObject.java
@@ -6,7 +6,6 @@ import de.gurkenlabs.litiengine.environment.tilemap.IMapObjectLayer;
 import de.gurkenlabs.litiengine.environment.tilemap.IMapObjectText;
 import de.gurkenlabs.litiengine.environment.tilemap.IPolyShape;
 import de.gurkenlabs.litiengine.environment.tilemap.ITilesetEntry;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
@@ -337,7 +336,7 @@ public class MapObject extends CustomPropertyProvider implements IMapObject {
   public float getY() {
     if (this.isInfiniteMap()) {
       TmxMap map = (TmxMap) this.getLayer().getMap();
-      return Nullability.castToNonnull(this.y) - map.getChunkOffsetY() * map.getTileHeight();
+      return this.y - map.getChunkOffsetY() * map.getTileHeight();
     }
 
     return this.y == null ? 0 : this.y;
